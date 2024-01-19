26f598558260544c550f041ffa92ccd3f58b8b67
lonehegelian <thelonehegelian@protonmail.com>
2024-01-19 05:45:13 +0000

    Add loadS3DataIntoPinecone function to create-chat route

--- src/app/api/create-chat/route.ts
+++ src/app/api/create-chat/route.ts
@@ -1,9 +1,11 @@
 import { NextResponse } from "next/server";
-
+import { loadS3DataIntoPinecone } from "../../../lib/pinecone";
 export async function POST(req: Request, res: Response) {
   try {
     const body = await req.json();
     const { fileKey, fileName } = body;
+    const pages = await loadS3DataIntoPinecone(fileKey);
+    return NextResponse.json({ pages })
   } catch (error) {
     console.log(error);
   }
