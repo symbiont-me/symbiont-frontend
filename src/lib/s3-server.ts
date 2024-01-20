d9deeb2e84f8db08ad27a8fb55a90803b2694bb0
lonehegelian <thelonehegelian@protonmail.com>
2024-01-20 20:12:27 +0000

    Fix S3 bucket name error in downloadFromS3 function

--- src/lib/s3-server.ts
+++ src/lib/s3-server.ts
@@ -1,24 +1,22 @@
 import AWS from "aws-sdk";
 import fs from "fs";
 export async function downloadFromS3(fileKey: string) {
+  
+  // @note removes the error in getObject(params) as bucket may not be set
+  const bucketName = process.env.NEXT_PUBLIC_S3_BUCKET_NAME;
+  if (!bucketName) {
+    throw new Error("S3 bucket name is not set in environment variables");
+  }
   try {
     AWS.config.update({
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID || undefined,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || undefined,
     });
-    const s3 = new AWS.S3(
-      {
-        const params = {
-          Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME || "",
-          Key: fileKey,
-        },
-        region = process.env.NEXT_PUBLIC_AWS_REGION || "";
-      }
-    );
+    const s3 = new AWS.S3();
     const params = {
-      Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME || "",
+      Bucket: bucketName,
       Key: fileKey,
-    }
+    };
 
     const data = await s3.getObject(params).promise();
     const file_name = `/tmp/pdf/${Date.now()}.pdf`;
