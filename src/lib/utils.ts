82eabc740baede367b24d61ad12440e6900f0dbd
lonehegelian <thelonehegelian@protonmail.com>
2024-01-21 02:29:57 +0000

    Add convertToAscii function to utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function convertToAscii(input: string): string {
+  // remove all non-ascii characters
+  return input.replace(/[^\x00-\x7F]/g, "")
+}
