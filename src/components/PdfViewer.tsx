c45ddcab1691d305f354a677b4114c803dbeb465
lonehegelian <thelonehegelian@protonmail.com>
2024-01-22 00:48:32 +0000

    Add PdfViewer component

---
+++ src/components/PdfViewer.tsx
@@ -0,0 +1,20 @@
+
+
+
+
+type Props = {
+    pdfUrl: string;
+
+}
+
+const PdfViewer = ({ pdfUrl }: Props) => {
+
+    
+    
+    return (
+      
+            <iframe src={`https://docs.google.com/gview?url=${pdfUrl}&embedded=true`} className="w-full h-full"></iframe>
+        )     
+}
+
+export default PdfViewer
