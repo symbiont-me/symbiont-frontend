819cdc0f804e7667cf9d99593af46fd4cb869a59
lonehegelian <thelonehegelian@protonmail.com>
2024-01-22 00:14:54 +0000

    Add ChatSideBar component

---
+++ src/components/ChatSideBar.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link'
+import { Button } from '../components/ui/Button'
+import { PlusCircle } from 'lucide-react'
+import {DrizzleChat} from '@/lib/db/schema'
+
+type Props = { 
+    chats: DrizzleChat[];
+    chatId: string;
+
+}
+
+const ChatSidebar =  ({chats, chatId}: Props) => {
+    return (
+        <div className="w-full h-screen p-4 text-gray-200 bg-gray-900">
+        <Link href = "/">
+            <Button>
+                <PlusCircle className='mr-2 w-4 h-4'/>
+                Create a new chat
+            </Button>
+        </Link>
+        <div>
+            {chats.map((chat) => {
+                console.log(chat)
+                return (
+                    <Link href={`/chat/${chat.id}`} key={chat.id} className={`block p-2 my-2 rounded-xl ${chat.id === parseInt(chatId) ? 'bg-gray-700' : ''}`}>
+                            {chat.pdfName}
+    
+                    </Link>
+                )
+            }
+            )}
+        </div>
+
+
+        </div>
+    )
+}
+
+export default ChatSidebar
