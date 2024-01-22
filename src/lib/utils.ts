486945742c826f3f29f3078c8455555c062b31c2
lonehegelian <thelonehegelian@protonmail.com>
2024-01-22 04:49:54 +0000

    Add cn function to utils.ts for combining class names

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+/**
+ * Combines class names into a single string with deduplication and conditional classes support.
+ * It uses `clsx` to parse the inputs and `twMerge` to merge Tailwind CSS classes intelligently.
+ * @param inputs - An array of class values which can be strings, objects, or arrays.
+ * @returns A single, merged string of class names.
+ * 
+ * Example:
+ * ```
+ * const buttonClass = cn('btn', { 'btn-primary': isPrimary, 'btn-large': isLarge });
+ * ```
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
