0eaed054f2d211e49155c65ee67c80cb896b20a4
lonehegelian <thelonehegelian@protonmail.com>
2024-01-25 09:31:36 +0000

    Add UserChatInput component

---
+++ src/components/UserChatInput.tsx
@@ -0,0 +1,33 @@
+"use client"
+import React, { useState } from "react";
+import { Input } from "./ui/Input";
+import { Button } from "./ui/Button";
+import { Send } from "lucide-react";
+
+
+type UserChatInputProps = {
+  input: string;
+  handleInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+}
+
+export default function UserChatInput ({input, handleInputChange, handleSubmit}: UserChatInputProps) {
+    return (
+        <form
+        onSubmit={handleSubmit}
+        className="sticky bottom-0 inset-x-0 px-2 py-4 bg-white"
+      >
+        <div className="flex">
+          <Input
+            value={input}
+            onChange={handleInputChange}
+            placeholder="Ask any question..."
+            className="w-full"
+          />
+          <Button className="bg-blue-600 ml-2">
+            <Send className="h-4 w-4" />
+          </Button>
+        </div>
+      </form>
+    )
+}
