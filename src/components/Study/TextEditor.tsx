f4544162a3579c1450ce356cfedcb8ebdc5e9e04
lonehegelian <thelonehegelian@protonmail.com>
2024-01-27 22:29:04 +0000

    Add TODO

--- src/components/Study/TextEditor.tsx
+++ src/components/Study/TextEditor.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
+// TODO autosave to db
 export default function TextEditor() {
   const [value, setValue] = useState('');
 
