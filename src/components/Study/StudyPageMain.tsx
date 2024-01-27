ff6be6149cbc35ac823aec1556bfbe6549dae7cc
lonehegelian <thelonehegelian@protonmail.com>
2024-01-27 22:10:28 +0000

    Add StudyPageMain and TextEditor components

---
+++ src/components/Study/StudyPageMain.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const StudyPageMain: React.FC = () => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default StudyPageMain;
+
+
+
+
---
+++ src/components/Study/TextEditor.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+export default function TextEditor() {
+  const [value, setValue] = useState('');
+
+  return <ReactQuill theme="snow" value={value} onChange={setValue} />;
+}
