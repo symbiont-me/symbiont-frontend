92f7c370f9763e715d1b695efc0fddc811dd769c
lonehegelian <thelonehegelian@protonmail.com>
2024-01-27 22:11:05 +0000

    Add StudyPage component with TextEditor

---
+++ src/app/study/[studyId]/page.tsx
@@ -0,0 +1,21 @@
+import TextEditor from "@/components/Study/TextEditor";
+
+import React from "react";
+
+// TODO add sidebar
+// TODO add chat component bar on the right
+// TODO add pdf viewer
+// TODO add video viewer
+// TODO add audio player
+// TODO add multiple choice quiz
+
+const StudyPage: React.FC = () => {
+  return (
+    <div>
+        <TextEditor />
+      
+    </div>
+  );
+};
+
+export default StudyPage;
