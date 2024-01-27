70d09ddd2fbf7bca595bd573fc4ea8fff7bb1eb6
lonehegelian <thelonehegelian@protonmail.com>
2024-01-27 22:29:18 +0000

    Add LLMSettings component

---
+++ src/components/Dashboard/LLMSettings.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LLMSettings: React.FC = () => {
+  // TODO: Implement LLMSettings component
+  return (
+    <div>
+      {/* Your LLMSettings component code goes here */}
+    </div>
+  );
+};
+
+export default LLMSettings;
