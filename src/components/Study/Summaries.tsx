ebbb6f5eac78d9b32dac45a94dd522b8520fc253
lonehegelian <thelonehegelian@protonmail.com>
2024-01-28 16:13:46 +0000

    Add Summaries component

---
+++ src/components/Study/Summaries.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ViewSelected } from '@/const/index';
+
+type SummariesProps = {
+  // Define any props you need for this component
+};
+
+const Summaries: React.FC<SummariesProps> = (props) => {
+  // Component logic goes here
+
+  return (
+    <div>
+      {/* Component layout and children go here */}
+      <h1>Summaries</h1>
+      <p>This is the Summaries component, where summaries will be displayed.</p>
+    </div>
+  );
+};
+
+export default Summaries;
