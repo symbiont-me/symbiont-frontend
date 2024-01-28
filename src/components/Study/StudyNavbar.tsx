fc0fc87b8c4e164c848928f757b094aab478bb01
lonehegelian <thelonehegelian@protonmail.com>
2024-01-28 13:34:48 +0000

    Refactor StudyNavbar component to use ViewSelected enum

--- src/components/Study/StudyNavbar.tsx
+++ src/components/Study/StudyNavbar.tsx
@@ -1,33 +1,18 @@
-"use client"
 import React from 'react';
-import Link from 'next/link';
-import { useRouter } from 'next/navigation';
+import { ViewSelected } from '@/const';
 
-// TODO should render new Components on the same page and not new Links
-
-const StudyNavbar = () => {
-  const router = useRouter();
-
+const Navigation: React.FC<{ setViewSelected: (view: ViewSelected) => void }> = ({ setViewSelected }) => {
   return (
     <nav className="flex justify-between items-center p-4 bg-gray-200">
       <div className="flex gap-4">
-        
-      <button onClick={() => router.push('/writer')} className={`text-sm font-medium ${router.pathname === '/upload' ? 'text-blue-500' : 'text-gray-700'}`}>
-          Writer
-        </button>
-
-        <button onClick={() => router.push('/pdfviewer')} className={`text-sm font-medium ${router.pathname === '/study' ? 'text-blue-500' : 'text-gray-700'}`}>
-          View PDF
-        </button>
-        <button onClick={() => router.push('/mcqtest')} className={`text-sm font-medium ${router.pathname === '/chat' ? 'text-blue-500' : 'text-gray-700'}`}>
-          Test
-        </button>
-        <button onClick={() => router.push('/scipapers')} className={`text-sm font-medium ${router.pathname === '/upload' ? 'text-blue-500' : 'text-gray-700'}`}>
-          Science Papers
-        </button>
+        {Object.values(ViewSelected).map(view => (
+          <button key={view} onClick={() => setViewSelected(view)}>
+            <p className='capitalize'>{view}</p>
+          </button>
+        ))}
       </div>
     </nav>
   );
 };
 
-export default StudyNavbar;
+export default Navigation;
