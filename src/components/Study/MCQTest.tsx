f38123d7d7190c21e1d6b1cb99dbe14c3cb6ed18
lonehegelian <thelonehegelian@protonmail.com>
2024-01-28 13:34:56 +0000

    Add MCQTest component

---
+++ src/components/Study/MCQTest.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MCQTest: React.FC = () => {
+  return (
+    <div>
+      <h1>Multiple Choice Questions Test</h1>
+      {/* Here you can render your MCQ test questions and options */}
+    </div>
+  );
+};
+
+export default MCQTest;
+
