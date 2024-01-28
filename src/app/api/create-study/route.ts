23f76509f7539ed2b824f53b1401cb4f66e1889c
lonehegelian <thelonehegelian@protonmail.com>
2024-01-28 01:32:27 +0000

    Refactor create-study route to insert new study into the database

--- src/app/api/create-study/route.ts
+++ src/app/api/create-study/route.ts
@@ -1,17 +1,30 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { db } from '@/lib/db'
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/lib/db";
+import { studies } from "@/lib/db/schema";
 
 export const POST = async (req: NextRequest, res: NextResponse) => {
-    try {
-        // Extract data from request body
-        const data = await req.json();
+  try {
+    // Extract data from request body
+    const { studyName, userId } = await req.json();
+    console.log(studyName);
 
-        // TODO: Add logic to handle data and create a new study
+    // Create a new study in the database
+    const studyId = await db
+      .insert(studies)
+      .values({
+        name: studyName,
+        userId: userId,
+      })
+      .returning({ insertedId: studies.id })
+      .execute();
 
-        // Placeholder for response
-        return NextResponse.json({ message: 'Study created successfully' });
-    } catch (error) {
-        console.error(error);
-        return NextResponse.json({ error: 'An error occurred while creating the study' }, { status: 500 });
-    }
-}
+    console.log(studyId);
+    return NextResponse.json({ message: "Study created successfully" });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "An error occurred while creating the study" },
+      { status: 500 }
+    );
+  }
+};
