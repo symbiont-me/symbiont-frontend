11bdad68d60af5b2168a464ea901723e44adcf2e
lonehegelian <thelonehegelian@protonmail.com>
2024-01-28 13:43:24 +0000

    Add new enums for ViewSelected

--- src/const/index.ts
+++ src/const/index.ts
@@ -2,3 +2,13 @@ export enum TextModels {
     GPT_3_5_TURBO = "gpt-3.5-turbo",
     GPT_4 = "gpt-4"
   }
+
+export enum ViewSelected {
+    Writer = 'writer',
+    PDFViewer = 'PDF viewer',
+    MCQTest = 'mcq test',
+    SciencePapers = 'sci papers',
+    Evaluation = 'evaluation',
+    VideoViewer = 'video viewer',
+    AudioPlayer = 'audio player',
+  }
