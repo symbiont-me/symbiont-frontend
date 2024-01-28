e393f85d1f51551bb61fdfa76c5f520516f76377
lonehegelian <thelonehegelian@protonmail.com>
2024-01-28 19:26:00 +0000

    Refactor ModelSelectDropdown component to fix warning

--- src/components/ModelSelectDropdown.tsx
+++ src/components/ModelSelectDropdown.tsx
@@ -2,21 +2,21 @@ import {TextModels} from '@/const';
 
 type Props = {
     setModel: (model: TextModels) => void;
-  };
+};
 
 export default function ModelSelectDropdown({setModel}:Props) {
   return (
     <div>
-        <select className = "select select-success w-full max-w-xs" onChange={(e) => setModel(e.target.value as TextModels)}>
-        <option disabled selected>Pick a Model</option>
+        <select 
+          className="select select-success w-full max-w-xs" 
+          onChange={(e) => setModel(e.target.value as TextModels)}
+          defaultValue="" 
+        >
+          <option value="" disabled>Pick a Model</option> {/* Removed selected from here */}
           {Object.values(TextModels).map((model) => (
             <option key={model} value={model}>{model}</option>
-            
-
           ))}
-          
         </select>
-      
     </div>
   )
 }
