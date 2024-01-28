7c08fd2077e6d2f846f517b076f3a3cfd2403a2e
lonehegelian <thelonehegelian@protonmail.com>
2024-01-28 16:51:56 +0000

    Refactor PdfViewer component

--- src/components/Study/PdfViewer.tsx
+++ src/components/Study/PdfViewer.tsx
@@ -1,20 +1,15 @@
-
-
-
-
 type Props = {
-    pdfUrl: string | undefined;
+  pdfUrl: string | undefined;
+};
 
-}
-
+// TODO allow users switch between  multiple pdfs
 const PdfViewer = ({ pdfUrl }: Props) => {
+  return (
+    <iframe
+      src={`https://docs.google.com/gview?url=${pdfUrl}&embedded=true`}
+      className="w-full h-full"
+    ></iframe>
+  );
+};
 
-    
-    
-    return (
-      
-            <iframe src={`https://docs.google.com/gview?url=${pdfUrl}&embedded=true`} className="w-full h-full"></iframe>
-        )     
-}
-
-export default PdfViewer
+export default PdfViewer;
