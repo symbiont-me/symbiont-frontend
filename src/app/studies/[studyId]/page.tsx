03c77b1414d759c4266fb1ac9d8dc85bca4a519d
lonehegelian <thelonehegelian@protonmail.com>
2024-01-28 13:35:21 +0000

    Add new components and implement dynamic rendering

--- src/app/studies/[studyId]/page.tsx
+++ src/app/studies/[studyId]/page.tsx
@@ -1,8 +1,47 @@
+"use client"
+import React from "react";
 import TextEditor from "@/components/Study/TextEditor";
 import ChatComponent from "@/components/ChatComponent";
 import StudyNavbar from "@/components/Study/StudyNavbar";
+import PdfViewer from "@/components/PdfViewer";
+import McqTest from "@/components/Study/MCQTest";
+import SciPapers from "@/components/Study/SciPapers";
+import { ViewSelected } from "@/const";
 
-import React from "react";
+// an object that maps each ViewSelected enum value to a corresponding React component.
+// this allows the application to dynamically render different components based on the current view selection
+const viewComponents: Record<ViewSelected, React.ComponentType<{ pdfUrl: string | undefined}>> = {
+  [ViewSelected.Writer]: TextEditor,
+  [ViewSelected.PDFViewer]: PdfViewer,
+  [ViewSelected.MCQTest]: McqTest, 
+  [ViewSelected.SciencePapers]: SciPapers, 
+};
+
+type StudyPageProps = {
+  pdfUrl: string | undefined;
+
+}
+
+const StudyPage: React.FC<StudyPageProps> = ({pdfUrl}: StudyPageProps) => {
+  const [viewSelected, setViewSelected] = React.useState<ViewSelected>(ViewSelected.Writer);
+
+  const SelectedViewComponent = viewComponents[viewSelected] || null;
+
+  return (
+    <div>
+      <StudyNavbar setViewSelected={setViewSelected} />
+      <div className="flex flex-row h-screen">
+        {/* <Sidebar /> */}
+        
+        {SelectedViewComponent && <SelectedViewComponent  pdfUrl={pdfUrl}/>}
+        <ChatComponent />
+      </div>
+    </div>
+  );
+};
+
+export default StudyPage;
+
 
 // TODO add sidebar
 // TODO add chat component bar on the right
@@ -10,20 +49,3 @@ import React from "react";
 // TODO add video viewer
 // TODO add audio player
 // TODO add multiple choice quiz
-
-const StudyPage: React.FC = () => {
-  return (
-    <div>
-      {/* TODO center this */}
-      <StudyNavbar />
-    <div className="flex flex-row h-screen">
-      
-        <TextEditor />
-        <ChatComponent />
-      
-    </div>
-    </div>
-  );
-};
-
-export default StudyPage;
