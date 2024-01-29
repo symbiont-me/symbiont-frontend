2a317b33011938f47dfe68b62a7d49c237f8cd67
lonehegelian <thelonehegelian@protonmail.com>
2024-01-29 20:27:27 +0000

    Update PdfViewer props to accept an array of pdf URLs

--- src/components/Study/PdfViewer.tsx
+++ src/components/Study/PdfViewer.tsx
@@ -1,9 +1,15 @@
+
+
+
 type Props = {
-  pdfUrl: string | undefined;
+  pdfUrl: string[] | undefined;
 };
 
+
 // TODO allow users switch between  multiple pdfs
-const PdfViewer = ({ pdfUrl }: Props) => {
+const PdfViewer =  ({ pdfUrl }: Props) => {
+  
+
   return (
     <iframe
       src={`https://docs.google.com/gview?url=${pdfUrl}&embedded=true`}
