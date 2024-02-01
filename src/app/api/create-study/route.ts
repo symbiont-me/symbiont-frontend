37de65eeef42ea28e2cf3422a557069bfc49d161
lonehegelian <thelonehegelian@protonmail.com>
2024-02-01 00:30:37 +0000

    Add studyImage and create studyText in POST route

--- src/app/api/create-study/route.ts
+++ src/app/api/create-study/route.ts
@@ -1,12 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/lib/db";
-import { studies } from "@/lib/db/schema";
+import { studies, studyTexts } from "@/lib/db/schema";
 
 export const POST = async (req: NextRequest, res: NextResponse) => {
   try {
     // Extract data from request body
-    const { studyName, userId } = await req.json();
-
+    const { studyName, userId, studyImage } = await req.json();
     // Create a new study in the database
     const studyId = await db
       .insert(studies)
@@ -17,13 +16,22 @@ export const POST = async (req: NextRequest, res: NextResponse) => {
       .returning({ insertedId: studies.id })
       .execute();
 
-    console.log(studyId);
+    // Create a new study text in the database
+    const studyTextId = await db
+      .insert(studyTexts)
+      .values({
+        content: "",
+        studyId: studyId[0].insertedId,
+      })
+      .returning({ insertedId: studyTexts.id })
+      .execute();
+
     return NextResponse.json({ studyId: studyId[0].insertedId }, { status: 201 });
   } catch (error) {
     console.error(error);
     return NextResponse.json(
       { error: "An error occurred while creating the study" },
-      { status: 500 }
+      { status: 500 },
     );
   }
 };
