ab9f644d95d64ebd0e3768e25032d15a47330b94
lonehegelian <thelonehegelian@protonmail.com>
2024-02-01 14:36:48 +0000

    Fix errors and add Resources component

--- src/app/studies/[studyId]/page.tsx
+++ src/app/studies/[studyId]/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import TextEditor from "@/components/Study/TextEditor";
 import ChatComponent from "@/components/ChatComponent/ChatComponentMain";
@@ -11,14 +11,13 @@ import AudioPlayer from "@/components/Study/AudioPlayer";
 import TestKnowledge from "@/components/Study/TestKnowledge";
 import TextEvaluation from "@/components/Study/TextEvaluation";
 import Summaries from "@/components/Study/Summaries";
+import Resources from "@/components/Study/Resources";
 import { ViewSelected } from "@/const";
-import {db} from "@/lib/db";
-import {studies, chats} from "@/lib/db/schema";
-import {eq} from "drizzle-orm";
-import axios from 'axios';
-import {useState, useEffect} from 'react';
-
-
+import { db } from "@/lib/db";
+import { studies, chats } from "@/lib/db/schema";
+import { eq } from "drizzle-orm";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 // TODO add sidebar
 // TODO add chat component bar on the right
@@ -27,46 +26,51 @@ import {useState, useEffect} from 'react';
 // TODO add audio player
 // TODO add multiple choice quiz
 
-
 // an object that maps each ViewSelected enum value to a corresponding React component.
 // this allows the application to dynamically render different components based on the current view selection
-const viewComponents: Record<ViewSelected, React.ComponentType<{ pdfUrl: string | undefined, textWriterValue: string, video_url:string | undefined}>> = {
+const viewComponents: Record<
+  ViewSelected,
+  React.ComponentType<{
+    pdfUrl: string | undefined;
+    textWriterValue: string;
+    video_url: string | undefined;
+  }>
+> = {
+  // TODO fix the errors
   [ViewSelected.Writer]: TextEditor,
   [ViewSelected.PDFViewer]: PdfViewer,
-  [ViewSelected.TestKnowledge]: TestKnowledge, 
-  [ViewSelected.SciencePapers]: SciPapers, 
-  [ViewSelected.Evaluation] : TextEvaluation,
+  [ViewSelected.TestKnowledge]: TestKnowledge,
+  [ViewSelected.SciencePapers]: SciPapers,
+  [ViewSelected.Evaluation]: TextEvaluation,
   [ViewSelected.VideoViewer]: VideoViewer,
   [ViewSelected.AudioPlayer]: AudioPlayer,
-  [ViewSelected.Summaries]: Summaries
+  [ViewSelected.Summaries]: Summaries,
+  [ViewSelected.Resources]: Resources,
 };
 
 type StudyPageProps = {
   pdfUrl: string | undefined;
+};
 
-}
-
-const StudyPage: React.FC<StudyPageProps> = ({pdfUrl}: StudyPageProps) => {
-  const [viewSelected, setViewSelected] =useState<ViewSelected>(ViewSelected.Writer);
+const StudyPage: React.FC<StudyPageProps> = ({ pdfUrl }: StudyPageProps) => {
+  const [viewSelected, setViewSelected] = useState<ViewSelected>(ViewSelected.Writer);
   const [pdfs, setPdfs] = useState<string[]>([]);
-  const [textWriterValue, setTextWriterValue] = useState<string>('');
+  const [textWriterValue, setTextWriterValue] = useState<string>("");
   const [video_url, setVideoUrl] = useState<string | undefined>(undefined);
-
 
   useEffect(() => {
     const fetchPDFs = async () => {
       try {
-        const response = await axios.post('/api/get-pdfs', { studyId: 1});
+        const response = await axios.post("/api/get-pdfs", { studyId: 1 });
         // TODO add pdfUrl to state
         setPdfs(response.data);
       } catch (error) {
-        console.error('Error fetching PDFs:', error);
+        console.error("Error fetching PDFs:", error);
       }
     };
 
     fetchPDFs();
   }, []);
-  
 
   const SelectedViewComponent = viewComponents[viewSelected] || null;
 
@@ -75,9 +79,8 @@ const StudyPage: React.FC<StudyPageProps> = ({pdfUrl}: StudyPageProps) => {
       <StudyNavbar setViewSelected={setViewSelected} />
       <div className="flex flex-row  ">
         {/* <Sidebar /> */}
-        
-        
-        {SelectedViewComponent && <SelectedViewComponent  pdfUrls={pdfs}/>}
+
+        {SelectedViewComponent && <SelectedViewComponent pdfUrls={pdfs} />}
         <ChatComponent />
       </div>
     </div>
@@ -85,5 +88,3 @@ const StudyPage: React.FC<StudyPageProps> = ({pdfUrl}: StudyPageProps) => {
 };
 
 export default StudyPage;
-
-
