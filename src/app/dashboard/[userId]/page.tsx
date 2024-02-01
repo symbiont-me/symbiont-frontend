ca5470f3df7d62bf2cd524c669c7db00c032d80f
lonehegelian <thelonehegelian@protonmail.com>
2024-02-01 14:37:12 +0000

    Add user dashboard component

--- src/app/dashboard/[userId]/page.tsx
+++ src/app/dashboard/[userId]/page.tsx
@@ -1,1 +1,12 @@
 // TODO render user Dashboard
+import React from 'react';
+
+const DashboardComponent = () => {
+  return (
+    <div>
+    <h1>User Dashboard</h1>
+    </div>
+  );
+};
+
+export default DashboardComponent;
