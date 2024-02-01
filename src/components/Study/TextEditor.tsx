3dfb4d27a64cb55a0853904c9dc66fe41f52adc2
lonehegelian <thelonehegelian@protonmail.com>
2024-02-01 00:30:23 +0000

    Refactor TextEditor component to use updated table and colum names

--- src/components/Study/TextEditor.tsx
+++ src/components/Study/TextEditor.tsx
@@ -1,57 +1,58 @@
-'use client'
-import React, { useState, useEffect } from 'react';
+"use client";
+import React, { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
-import 'react-quill/dist/quill.snow.css';
-import axios from 'axios';
-import {useAuth} from '@clerk/nextjs';
+import "react-quill/dist/quill.snow.css";
+import axios from "axios";
+import { useAuth } from "@clerk/nextjs";
 
 // TODO on render, fetch text from db and set value to that text
 
 // NOTE this fixes the issue with document not being defined at initial render
 // ref: https://github.com/zenoamaro/react-quill/issues/292
-const ReactQuill = dynamic(() => import('react-quill'), {
+const ReactQuill = dynamic(() => import("react-quill"), {
   ssr: false,
-  loading: () => <div className="flex justify-center items-center h-screen"><span className="loading loading-dots loading-md"></span></div>,
+  loading: () => (
+    <div className="flex justify-center items-center h-screen">
+      <span className="loading loading-dots loading-md"></span>
+    </div>
+  ),
 });
-
-
 
 export default function TextEditor() {
   const auth = useAuth();
-  const [value, setValue] = useState('');
+  const [text, setText] = useState("");
   if (!auth) {
-    return <div>auth is undefined</div>
+    return <div>auth is undefined</div>;
   }
-  
+
   useEffect(() => {
     const saveText = async () => {
       try {
-        const response = await axios.post('/api/save-writer-text', {
-          textId: 1,
+        const response = await axios.post("/api/save-writer-text", {
+          studyTextId: 1,
           studyId: 1,
           userId: auth.userId,
-          textContent: value
+          studyTextContent: text,
         });
-        console.log(response.data);
+        console.log("response", response.status);
+        if (response.status === 200) {
+          console.log("text saved");
+        }
       } catch (error) {
-        console.error('Error saving text:', error);
+        throw error;
       }
     };
-    
+
     const timer = setTimeout(() => {
       saveText();
-      console.log('text saved');
     }, 10000);
 
     return () => clearTimeout(timer);
-  }, [value]);
-    
-
-   
+  }, [text]);
 
   return (
-    <div className='w-full h-screen'>
-      <ReactQuill className='h-screen' theme="snow" value={value} onChange={setValue} />
+    <div className="w-full h-screen">
+      <ReactQuill className="h-screen" theme="snow" value={text} onChange={setText} />
     </div>
   );
 }
