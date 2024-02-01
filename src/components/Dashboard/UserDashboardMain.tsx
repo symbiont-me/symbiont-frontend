f57405b1562d3dd6d12e409596c8598c282db2cb
lonehegelian <thelonehegelian@protonmail.com>
2024-02-01 01:03:45 +0000

    Add API call to fetch projects in UserDashboard

--- src/components/Dashboard/UserDashboardMain.tsx
+++ src/components/Dashboard/UserDashboardMain.tsx
@@ -1,19 +1,49 @@
+'use client'
 import LeftSidebar from "../LeftSidebar";
 import ProjectCard from "./StudyCard";
 import NewStudyCard from "./NewStudyCard";
-
+import { useState, useEffect} from "react";
+import axios from "axios";
 // TODO get projects from db
 // TODO handle errors in case of no projects
 
-export default function UserDashboard() {
+
+type UserDashboardProps = {
+    userId: string;
+};
+
+
+export default function UserDashboard({userId}: UserDashboardProps) {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => { 
+        async function fetchProjects() {
+            try {
+                const response = await axios.post('/api/get-studies', { userId });
+                console.log('response', response);
+                setProjects(response.data);
+
+            } catch (error) {
+                console.error('Error fetching projects:', error);
+            }
+        }
+
+        fetchProjects();
+
+    }, []);    
+
     return (
         <div className = "m-10">
             {/* Left Sidebar */}
             {/* Center Dashboard */}
             <div className="">
             <div className="grid grid-cols-4 gap-4">
-                <ProjectCard title="test" img="" description=""/>
-           
+           {
+                projects.map((project) => (
+                     <ProjectCard title={project.title} img={project.img} description={project.description}/>
+                ))  
+           }
+             
                 <div className="border p-4">
                     <NewStudyCard/>
                 </div>
