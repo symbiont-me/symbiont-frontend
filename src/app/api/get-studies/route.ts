dec3f6d2365e5cd10ff5b44d1b4df8bc9055b5ed
lonehegelian <thelonehegelian@protonmail.com>
2024-02-01 00:30:45 +0000

    Add route to get studies by user ID

---
+++ src/app/api/get-studies/route.ts
@@ -0,0 +1,10 @@
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/lib/db";
+import { studies } from "@/lib/db/schema";
+import { eq } from "drizzle-orm";
+
+export const POST = async (req: NextRequest, res: NextResponse) => {
+  const { userId } = await req.json();
+    const _studies = await db.select().from(studies).where(eq(studies.userId, userId ))
+    return NextResponse.json(_studies)
+};
