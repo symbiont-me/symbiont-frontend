2cd15a7b30d7a9040a3487be29a82a3dd9885cb6
lonehegelian <thelonehegelian@protonmail.com>
2024-02-01 01:01:28 +0000

    Add usePathname hook and fix studyId in saveText function

--- src/components/Study/TextEditor.tsx
+++ src/components/Study/TextEditor.tsx
@@ -4,7 +4,7 @@ import dynamic from "next/dynamic";
 import "react-quill/dist/quill.snow.css";
 import axios from "axios";
 import { useAuth } from "@clerk/nextjs";
-
+import { usePathname } from "next/navigation";
 // TODO on render, fetch text from db and set value to that text
 
 // NOTE this fixes the issue with document not being defined at initial render
@@ -21,20 +21,24 @@ const ReactQuill = dynamic(() => import("react-quill"), {
 export default function TextEditor() {
   const auth = useAuth();
   const [text, setText] = useState("");
+
+  // NOTE I don't like doing it this way
+  const path = usePathname();
+  const studyId = path.split("/")[2];
+
   if (!auth) {
-    return <div>auth is undefined</div>;
+    return <div>Please Log in </div>;
   }
 
   useEffect(() => {
     const saveText = async () => {
       try {
         const response = await axios.post("/api/save-writer-text", {
-          studyTextId: 1,
-          studyId: 1,
+          // TODO fix studyTextId and studyId
+          studyId: studyId,
           userId: auth.userId,
           studyTextContent: text,
         });
-        console.log("response", response.status);
         if (response.status === 200) {
           console.log("text saved");
         }
