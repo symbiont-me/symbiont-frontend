3c0b8861ce3ce8413d1bde6c725a255044faa658
lonehegelian <thelonehegelian@protonmail.com>
2024-02-01 14:33:40 +0000

    Update enum to render Resources component

--- src/const/index.ts
+++ src/const/index.ts
@@ -1,15 +1,16 @@
 export enum TextModels {
-    GPT_3_5_TURBO = "gpt-3.5-turbo",
-    GPT_4 = "gpt-4"
-  }
+  GPT_3_5_TURBO = "gpt-3.5-turbo",
+  GPT_4 = "gpt-4",
+}
 
 export enum ViewSelected {
-    Writer = 'writer',
-    PDFViewer = 'PDF viewer',
-    TestKnowledge = 'test knowledge',
-    SciencePapers = 'sci papers',
-    Evaluation = 'evaluation',
-    VideoViewer = 'video viewer',
-    AudioPlayer = 'audio player',
-    Summaries = 'summaries',
-  }
+  Writer = "writer",
+  PDFViewer = "PDF viewer",
+  TestKnowledge = "test knowledge",
+  SciencePapers = "sci papers",
+  Evaluation = "evaluation",
+  VideoViewer = "video viewer",
+  AudioPlayer = "audio player",
+  Summaries = "summaries",
+  Resources = "resources",
+}
