dd772e8449ea7c7a0ec90cac7692c4c449aed10c
lonehegelian <thelonehegelian@protonmail.com>
2024-02-02 01:52:10 +0000

    Refactor PdfViewer component to fetch and display multiple PDFs

--- src/components/Study/PdfViewer.tsx
+++ src/components/Study/PdfViewer.tsx
@@ -1,20 +1,37 @@
-
-
-
+import { useEffect, useState } from "react";
+import axios from "axios";
+import {StudyResource} from "@/app/types";
 type Props = {
   pdfUrl: string[] | undefined;
 };
 
-
 // TODO allow users switch between  multiple pdfs
-const PdfViewer =  ({ pdfUrl }: Props) => {
-  
+const PdfViewer = ({ pdfUrl }: Props) => {
+  const [pdf, setPdf] = useState<StudyResource[]>([]);
+  useEffect(() => {
+    const getPdfs = async () => {
+      const response = await axios.post("/api/get-pdfs", {
+        studyId: 1,
+        category: "pdf",
+      });
+
+      setPdf(response.data);
+    };
+    getPdfs();
+    console.log(pdf);
+  }, []);
 
   return (
-    <iframe
-      src={`https://docs.google.com/gview?url=${pdfUrl}&embedded=true`}
-      className="w-full h-full"
-    ></iframe>
+    <div className = "flex flex-col">
+      
+      {pdf.length > 0 && (
+        <iframe
+          src={`https://docs.google.com/gview?url=${pdf[0].url}&embedded=true`}
+          className="w-full h-full"
+        ></iframe>
+      )}
+
+    </div>
   );
 };
 
