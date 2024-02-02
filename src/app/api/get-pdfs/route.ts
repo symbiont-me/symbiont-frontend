3eee39dd8ee72d744471f798c8c53b4c81487478
lonehegelian <thelonehegelian@protonmail.com>
2024-02-02 01:52:32 +0000

    Update get-pdfs route to fetch study resources by category

--- src/app/api/get-pdfs/route.ts
+++ src/app/api/get-pdfs/route.ts
@@ -1,12 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { db } from '@/lib/db'
-import { chats } from '@/lib/db/schema'
+import { studies, studyResources } from '@/lib/db/schema'
 import { eq } from 'drizzle-orm'
 
 export const POST = async (req: NextRequest, res: NextResponse) => {
     const {studyId} = await req.json()
 
-    const _pdfs = await db.select().from(chats).where(eq(chats.studyId, 1))
+    const _pdfs = await db.select().from(studyResources).where(eq(studyId, studyId) && eq(studyResources.category, 'pdf'))
 
     return NextResponse.json(_pdfs)
 }
