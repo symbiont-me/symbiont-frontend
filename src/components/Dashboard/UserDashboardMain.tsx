24d4c775bdf11bfab6671b04a893396deac28535
lonehegelian <thelonehegelian@protonmail.com>
2024-02-03 22:34:03 +0000

    Refactor UserDashboardMain component with correct types

--- src/components/Dashboard/UserDashboardMain.tsx
+++ src/components/Dashboard/UserDashboardMain.tsx
@@ -1,55 +1,51 @@
-'use client'
+"use client";
 import LeftSidebar from "../LeftSidebar";
 import ProjectCard from "./StudyCard";
 import NewStudyCard from "./NewStudyCard";
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
-// TODO get projects from db
-// TODO handle errors in case of no projects
+import { Study } from "@/app/types";
 
-
+// TODO use react query to fetch the projects
 type UserDashboardProps = {
-    userId: string;
+  userId: string;
 };
 
+export default function UserDashboard({ userId }: UserDashboardProps) {
+  const [projects, setProjects] = useState<Study[]>([]);
 
-export default function UserDashboard({userId}: UserDashboardProps) {
-    const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    async function fetchProjects() {
+      try {
+        const response = await axios.post("/api/get-studies", { userId });
+        setProjects(response.data);
+      } catch (error) {
+        console.error("Error fetching projects:", error);
+      }
+    }
 
-    useEffect(() => { 
-        async function fetchProjects() {
-            try {
-                const response = await axios.post('/api/get-studies', { userId });
-                console.log('response', response);
-                setProjects(response.data);
+    fetchProjects();
+  }, []);
 
-            } catch (error) {
-                console.error('Error fetching projects:', error);
-            }
-        }
+  return (
+    <div className="m-10">
+      {/* Left Sidebar */}
+      {/* Center Dashboard */}
+      <div className="">
+        <div className="grid grid-cols-4 gap-4">
+          {projects.map((project) => (
+            <ProjectCard
+              key={project.id}
+              name={project.name}
+              image={project.image}
+            />
+          ))}
 
-        fetchProjects();
-
-    }, []);    
-
-    return (
-        <div className = "m-10">
-            {/* Left Sidebar */}
-            {/* Center Dashboard */}
-            <div className="">
-            <div className="grid grid-cols-4 gap-4">
-           {
-                projects.map((project) => (
-                     <ProjectCard title={project.title} img={project.img} description={project.description}/>
-                ))  
-           }
-             
-                <div className="border p-4">
-                    <NewStudyCard/>
-                </div>
-                
-            </div>
-            </div>
+          <div className="border p-4">
+            <NewStudyCard />
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
