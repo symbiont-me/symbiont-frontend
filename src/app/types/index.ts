25baee09c9cd66cfee85e069924959c866bac922
lonehegelian <thelonehegelian@protonmail.com>
2024-02-03 22:35:05 +0000

    Update StudyResource and Study types

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -1,18 +1,26 @@
 // NOTE not using Drizzle types because backend will be separated in python
 // need to update this to match the actual schema
 export type StudyResource = {
-    studyId: number;
-    id?: number;
-    name: string;
-    url: string;
-    identifier: string;
-    category: StudyResourceCategory;
-    createdAt?: Date;
-}
+  studyId: number;
+  id?: number;
+  name: string;
+  url: string;
+  identifier: string;
+  category: StudyResourceCategory;
+  createdAt?: Date;
+};
 
 export enum StudyResourceCategory {
-    PDF = "pdf",
-    Audio = "audio",
-    Video = "video",
-    Webpage = "webpage"
+  PDF = "pdf",
+  Audio = "audio",
+  Video = "video",
+  Webpage = "webpage",
 }
+
+export type Study = {
+  id?: number;
+  name: string;
+  image: string;
+  createdAt?: Date;
+  userId?: string;
+};
