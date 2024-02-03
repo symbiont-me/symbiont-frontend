910c9099ccd25d08363305b9241e074e9f55f240
lonehegelian <thelonehegelian@protonmail.com>
2024-02-03 22:34:58 +0000

    Refactor ResourceSwitcher component to use StudyResource object for selectedResource

--- src/components/ResourceSwitcher.tsx
+++ src/components/ResourceSwitcher.tsx
@@ -5,29 +5,32 @@ import { usePathname } from "next/navigation";
 import { StudyResource } from "@/app/types";
 
 type ResourceSwitcherProps = {
-  selectedResource: (resourceUrl: string) => void;
+  selectedResource: (resource: StudyResource) => void;
 };
 
 const ResourceSwitcher: React.FC<ResourceSwitcherProps> = ({
   selectedResource,
 }) => {
     // TODO add the updated study resource type
-  const [resources, setResources] = useState([]);
+  const [resources, setResources] = useState<StudyResource[]>([]);
   const path = usePathname();
   const studyId = path.split("/")[2];
 
   const handleResourceChange = (
     event: React.ChangeEvent<HTMLSelectElement>
   ) => {
-    const resourceUrl = event.target.value;
-    selectedResource(resourceUrl);
+    const resourceIdentifier = event.target.value;
+    // TODO there must be a better way to set the resource I am missing something obvious
+    const resource = resources.find(r => r.identifier === resourceIdentifier);
+    if (resource) {
+      selectedResource(resource);
+    }
   };
 
   useEffect(() => {
     async function fetchResources() {
       try {
         const response = await axios.post("/api/get-resources", { studyId });
-        console.log("response", response.data);
         setResources(response.data);
       } catch (error) {
         console.error("Error fetching projects:", error);
@@ -47,8 +50,8 @@ const ResourceSwitcher: React.FC<ResourceSwitcherProps> = ({
         >
           {resources.map((resource) => (
             <option
-              key={resource.id}
-              value={resource.name}
+              key={resource.identifier}
+              value={resource.identifier}
             >
               {resource.name}
             </option>
