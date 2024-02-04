8ffeb2fb346e3d414aebf4a1d6e47934e44719a3
lonehegelian <thelonehegelian@protonmail.com>
2024-02-04 21:59:40 +0000

    stndardise component

--- src/components/Dashboard/LLMSettings.tsx
+++ src/components/Dashboard/LLMSettings.tsx
@@ -1,12 +1,8 @@
 import React from "react";
 
-const LLMSettings: React.FC = () => {
+function LLMSettings() {
   // TODO: Implement LLMSettings component
-  return (
-    <div>
-      {/* Your LLMSettings component code goes here */}
-    </div>
-  );
-};
+  return <div>{/* Your LLMSettings component code goes here */}</div>;
+}
 
 export default LLMSettings;
