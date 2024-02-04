bdfd81fd95f5c4c3447ba5210b324c9fc9d1dfad
lonehegelian <thelonehegelian@protonmail.com>
2024-02-04 22:34:49 +0000

    update chat styles

--- src/components/ChatComponent/ChatComponentMain.tsx
+++ src/components/ChatComponent/ChatComponentMain.tsx
@@ -3,20 +3,20 @@ import { useChat, Message } from "ai/react";
 import MessageList from "@/components/ChatComponent/MessageList";
 import UserChatInput from "@/components/ChatComponent/UserChatInput";
 import { useState, useEffect } from "react";
-import ModelSelectDropdown from "@/components/ModelSelectDropdown";
 import { TextModels } from "@/const";
-import ApiKeyInput from "@/components/ApiKeyInput";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import ResourceSwitcher from "@/components/ResourceSwitcher";
 import { StudyResource } from "@/app/types";
+
 type ChatComponentProps = {
   chatId: number | undefined;
 };
 
 // TODO fix on load selectedResource is undefined even though it is set in the ResourceSwitcher
 // TODO model selection and api key input should be on the Dashboard
-const ChatComponent = (chatId: ChatComponentProps) => {
+
+function ChatComponent(chatId: ChatComponentProps) {
   const { data, isLoading } = useQuery({
     queryKey: ["chat", chatId],
 
@@ -28,20 +28,18 @@ const ChatComponent = (chatId: ChatComponentProps) => {
     },
   });
   const [selectedModel, setSelectedModel] = useState<TextModels>(
-    TextModels.GPT_3_5_TURBO
+    TextModels.GPT_3_5_TURBO,
   );
 
   // NOTE this is used to switch the context for the chat
   const [selectedResource, setSelectedResource] = useState<
     StudyResource | undefined
   >(undefined);
-  const [apiKey, setApiKey] = useState<string>("");
+  console.log(selectedResource);
   const { messages, input, handleInputChange, handleSubmit } = useChat({
     api: "/api/send-chat-message",
     body: {
       chatId,
-      // model: selectedModel,
-      // apiKey: apiKey,
       resourceIdentifier: selectedResource?.identifier || "",
     },
     initialMessages: data || [],
@@ -52,24 +50,22 @@ const ChatComponent = (chatId: ChatComponentProps) => {
     if (messageContainer) {
       messageContainer.scrollTo(0, messageContainer.scrollHeight);
     }
-  }, [messages]);
+  }, [messages, selectedResource]);
 
   return (
-    <div className="max-h-screen overflow-hidden w-1/2" id="message-container">
-      {/* header */}
-      <div className="sticky top-0 inset-x-0 p-2 bg-white h-fit">
-        <div className="flex flex-row items-center justify-between mx-4 my-2 space-x-4">
-          {/* <ModelSelectDropdown setModel={setSelectedModel} /> */}
-          {/* <ApiKeyInput setApiKey={setApiKey} /> */}
-          <ResourceSwitcher selectedResource={setSelectedResource} />
-        </div>
+    <div className="h-full p-2 overflow-hidden">
+      <div className="h-20 flex flex-col justify-center pr-20 pl-20">
+        <ResourceSwitcher selectedResource={setSelectedResource} />
+      </div>
+      <div
+        className="h-screen overflow-auto p-2 bg-slate-100"
+        id="message-container"
+      >
+        <MessageList messages={messages} />
       </div>
 
-      {/* message list */}
-      <MessageList messages={messages} />
-
-      {/* input */}
-      <div className="sticky bottom-12 inset-x-0 px-2 py-4 ">
+      {/* Input */}
+      <div className="h-20 flex flex-col justify-center pr-20 pl-20">
         <UserChatInput
           input={input}
           handleInputChange={handleInputChange}
@@ -78,6 +74,6 @@ const ChatComponent = (chatId: ChatComponentProps) => {
       </div>
     </div>
   );
-};
+}
 
 export default ChatComponent;
