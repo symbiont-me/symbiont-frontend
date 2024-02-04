cfe073ee7b5554c2248f8abfcfee847a8140812a
lonehegelian <thelonehegelian@protonmail.com>
2024-02-04 21:04:02 +0000

    reformat the component and add goto study link

--- src/components/Dashboard/StudyCard.tsx
+++ src/components/Dashboard/StudyCard.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Study } from "@/app/types";
-
+import Link from "next/link";
 type StudyCardProps = Study;
 
-const StudyCard: React.FC<StudyCardProps> = ({ name, image }) => {
+// TODO replace img with Image
+
+function StudyCard({ name, image, id }: StudyCardProps) {
   return (
     <div className="project-card">
       <div className="card lg:card-side bg-base-100 shadow-xl">
@@ -14,12 +16,14 @@ const StudyCard: React.FC<StudyCardProps> = ({ name, image }) => {
           <h2 className="card-title">{name}</h2>
           <div className="card-actions justify-end">
             {/* TODO on click redirect the user to the project */}
-            <button className="btn btn-primary">Open</button>
+            <Link href={`studies/${id}`} className="btn btn-primary">
+              Open
+            </Link>
           </div>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default StudyCard;
