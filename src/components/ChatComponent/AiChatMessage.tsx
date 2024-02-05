422af09ed6a0e744ed62b66f4775a23da3d013d3
lonehegelian <thelonehegelian@protonmail.com>
2024-02-05 23:20:37 +0000

    remove div

--- src/components/ChatComponent/AiChatMessage.tsx
+++ src/components/ChatComponent/AiChatMessage.tsx
@@ -3,10 +3,10 @@ import Markdown from "marked-react";
 // TODO fix markdown rendeing, bullet points are not rendering as list. There doesn't seem to be any markdown rendered
 const AiChatMessage = ({ message }: { message: string }) => {
   return (
-    <div>
+    <>
       <Markdown gfm={true} breaks={true} value={message} />
-    </div>
+    </>
   );
-}
+};
 
 export default AiChatMessage;
