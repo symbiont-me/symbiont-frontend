cf3c38a0c0c929754f219e9d82bab3c934270960
lonehegelian <thelonehegelian@protonmail.com>
2024-02-05 02:23:25 +0000

    Remove unused code and update dependencies

--- src/app/studies/[studyId]/page.tsx
+++ src/app/studies/[studyId]/page.tsx
@@ -4,7 +4,6 @@ import TextEditor from "@/components/Study/TextEditor";
 import ChatComponent from "@/components/ChatComponent/ChatComponentMain";
 import StudyNavbar from "@/components/Study/StudyNavbar";
 import PdfViewer from "@/components/Study/PdfViewer";
-import McqTest from "@/components/Study/TestKnowledge";
 import SciPapers from "@/components/Study/SciPapers";
 import VideoViewer from "@/components/Study/VideoViewer";
 import AudioPlayer from "@/components/Study/AudioPlayer";
@@ -14,16 +13,10 @@ import Summaries from "@/components/Study/Summaries";
 import Resources from "@/components/Study/Resources";
 import { ViewSelected } from "@/const";
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState} from "react";
 import { usePathname } from "next/navigation";
 import "../../../app/styles.css";
 
-// TODO add sidebar
-// TODO add chat component bar on the right
-// TODO add pdf viewer
-// TODO add video viewer
-// TODO add audio player
-// TODO add multiple choice quiz
 
 // an object that maps each ViewSelected enum value to a corresponding React component.
 // this allows the application to dynamically render different components based on the current view selection
@@ -47,15 +40,11 @@ const viewComponents: Record<
   [ViewSelected.Resources]: Resources,
 };
 
-type StudyPageProps = {
-  pdfUrl: string | undefined;
-};
-
-const StudyPage: React.FC<StudyPageProps> = ({ pdfUrl }: StudyPageProps) => {
+function StudyPage() {
   const [viewSelected, setViewSelected] = useState<ViewSelected>(
-    ViewSelected.Writer,
+    ViewSelected.Writer
   );
-  const [pdfs, setPdfs] = useState<string[]>([]);
+  // TODO Test and remove the following unused state variables
   const [textWriterValue, setTextWriterValue] = useState<string>("");
   const [video_url, setVideoUrl] = useState<string | undefined>(undefined);
   const path = usePathname();
@@ -75,21 +64,21 @@ const StudyPage: React.FC<StudyPageProps> = ({ pdfUrl }: StudyPageProps) => {
   };
   fetchLinkedChat();
 
-const SelectedViewComponent = viewComponents[viewSelected] || null;
+  const SelectedViewComponent = viewComponents[viewSelected] || null;
 
-return (
-  <div className="study-container w-full h-screen">
-    <div className="study-nav">
-      <StudyNavbar setViewSelected={setViewSelected} />
+  return (
+    <div className="study-container w-full h-screen">
+      <div className="study-nav">
+        <StudyNavbar setViewSelected={setViewSelected} />
+      </div>
+      <div className="view-container">
+        {SelectedViewComponent && <SelectedViewComponent studyId={studyId} />}
+      </div>
+      <div className="chat-container">
+        <ChatComponent chatId={chatId} />
+      </div>
     </div>
-    <div className="view-container">
-      {SelectedViewComponent && <SelectedViewComponent studyId={studyId} />}
-    </div>
-    <div className="chat-container">
-      <ChatComponent chatId={chatId} />
-    </div>
-  </div>
-);
-};
+  );
+}
 
 export default StudyPage;
