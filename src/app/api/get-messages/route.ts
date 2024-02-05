8d6d9792ef5205d14c73a41bafaf763d90ad58da
lonehegelian <thelonehegelian@protonmail.com>
2024-02-05 19:39:45 +0000

    fix undefined chatId issue

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -1,12 +1,14 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { db } from '@/lib/db'
-import { messages } from '@/lib/db/schema'
-import { eq } from 'drizzle-orm'
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/lib/db";
+import { messages } from "@/lib/db/schema";
+import { eq } from "drizzle-orm";
 
-export const POST = async (req: NextRequest, res: NextResponse) => {
-    const {chatId} = await req.json()
+export const POST = async (req: NextRequest) => {
+    const { chatId } = await req.json();
+    const _messages = await db
+        .select()
+        .from(messages)
+        .where(eq(messages.chatId, chatId));
 
-    const _messages = await db.select().from(messages).where(eq(messages.chatId, chatId.chatId))
-
-    return NextResponse.json(_messages)
-}
+    return NextResponse.json(_messages);
+};
