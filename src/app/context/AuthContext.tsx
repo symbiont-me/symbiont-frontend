b98c2c02703b359ce334a99719e793c8a73c8eb8
lonehegelian <thelonehegelian@protonmail.com>
2024-02-07 22:24:26 +0000

    Add useRouter hook and redirect user to "/" if not signed in

--- src/app/context/AuthContext.tsx
+++ src/app/context/AuthContext.tsx
@@ -1,8 +1,9 @@
-import { Auth } from "aws-sdk/clients/docdbelastic";
+"use client"
 import { app, auth } from "../../firebase/config";
 import { onAuthStateChanged, User  } from "firebase/auth";
 import { useContext } from "react";
 import { useEffect, useState, createContext , ReactNode} from "react";
+import {useRouter} from "next/navigation";
 
 type AuthContextType = {
   user: User | null;
@@ -13,25 +14,33 @@ export const AuthContext = createContext<AuthContextType | null>(null);
 export const useAuthContext = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }: {children: ReactNode}) => {
-
+const router = useRouter();
   console.log("AuthProvider");
   const [user, setUser] = useState<User | null>(null);
-  const [loading, setLoading] = useState(true);
-
+  const [loading, setLoading] = useState(false);
+  console.log(user);
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         setUser(user);
-        setLoading(false);
       } else {
+        router.push("/");
         setUser(null);
       }
+      setLoading(false);
     });
     return () => unsubscribe();
   }, []);
+
+    if (loading) {
+      return <div>Loading...</div>;
+    }
   return (
+
+
+    // TODO if user is not signed in, redirect to /sign-in
     <AuthContext.Provider value={{ user }}>
-      {loading ? <div>Loading...</div> : children}
+      {children}
     </AuthContext.Provider>
   );
 };
