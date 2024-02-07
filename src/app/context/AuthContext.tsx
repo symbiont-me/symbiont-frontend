cfe67b390501b33cc744b6c3f4276b0eef95ce29
lonehegelian <thelonehegelian@protonmail.com>
2024-02-07 21:55:05 +0000

    Add AuthContext component

---
+++ src/app/context/AuthContext.tsx
@@ -0,0 +1,35 @@
+import { Auth } from "aws-sdk/clients/docdbelastic";
+import { app, auth } from "../../firebase/config";
+import { onAuthStateChanged, User  } from "firebase/auth";
+import { useContext } from "react";
+import { useEffect, useState, createContext , ReactNode} from "react";
+
+type AuthContextType = {
+  user: User | null;
+}
+
+export const AuthContext = createContext<AuthContextType | null>(null);
+
+export const useAuthContext = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }: {children: ReactNode}) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+        setLoading(false);
+      } else {
+        setUser(null);
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {loading ? <div>Loading...</div> : children}
+    </AuthContext.Provider>
+  );
+};
