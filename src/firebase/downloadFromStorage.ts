66793b2fd1c93dc684a2102eee8fbbc29a11fbbd
lonehegelian <thelonehegelian@protonmail.com>
2024-02-09 23:46:46 +0000

    Refactor downloadFromStorage function and add deleteFileFromFolder function

--- src/firebase/downloadFromStorage.ts
+++ src/firebase/downloadFromStorage.ts
@@ -1,42 +1,38 @@
-import { getStorage, ref, getDownloadURL } from "firebase/storage";
 import axios from "axios";
-import { promises as fs } from "fs";
-import {app} from "@/firebase/config";
+import * as fs from "fs";
+import { getUrl } from "@/firebase/utils";
 
 // TODO initialise the firebase app somewhere on the upper level of the app
 // TODO make the file is secured and only accessible to the user who uploaded it
 
-async function getUrl(fileKey: string): Promise<string> {
-  const storage = getStorage(app);
-  const gsReference = ref(storage, `${process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET}/uploads/${fileKey}`);
-  try {
-    const url = await getDownloadURL(gsReference);
-    console.log("URL: ", url);
-    return url;
-  } catch (error) {
-    console.log(error);
-    throw new Error("Error downloading file from Firebase Storage");
-  }
-}
-
-
-
 export async function downloadFromStorage(url: string): Promise<string> {
   try {
-    const urlResponse = await getUrl(url);
-    const res = await axios.get(urlResponse, {
-      responseType: "blob",
+    const fileExtension = url.split(".").pop(); // probably wrong
+    const fileName = `symbiont-${Date.now().toString()}.${fileExtension}`;
+    const fileStream = fs.createWriteStream(fileName);
+    const downloadLink = await getUrl(url);
+    const res = await axios.get(downloadLink, {
+      responseType: "stream",
     });
 
-    const blob = res.data;
-    const fileName = `/tmp/symbiont-${Date.now().toString()}.pdf`;
-
-    // Write the file to disk
-    await fs.writeFile(fileName, blob);
+    res.data.pipe(fileStream);
+    await new Promise((resolve, reject) => {
+      fileStream.on("finish", resolve);
+      fileStream.on("error", reject);
+    });
     console.log(`File downloaded and saved as ${fileName}`);
     return fileName;
   } catch (error) {
     console.error("Error downloading or saving the file:", error);
     throw error;
+  }
+}
+
+export async function deleteFileFromFolder(fileName: string) {
+  try {
+    await fs.promises.unlink(fileName);
+    console.log(`Deleted file: ${fileName}`);
+  } catch (error) {
+    console.error(`Error deleting file ${fileName}:`, error);
   }
 }
