deee9808dc0a1119e82c1c282cdd9256a36aaf4e
lonehegelian <thelonehegelian@protonmail.com>
2024-02-10 17:58:38 +0000

    chore formatting and todo

--- src/components/Study/VideoViewer.tsx
+++ src/components/Study/VideoViewer.tsx
@@ -1,49 +1,51 @@
-
-import {useState, useEffect} from 'react';
-import YouTube, { YouTubeProps } from 'react-youtube';
+import { useState, useEffect } from "react";
+import YouTube, { YouTubeProps } from "react-youtube";
 
 type VideoViewerProps = {
   studyId: string | undefined;
-}
+};
 
-const  VideoViewer = ({studyId}: VideoViewerProps) => {
+// TODO get videos from the studyId and display them
+const VideoViewer = ({ studyId }: VideoViewerProps) => {
   const [videoId, setVideoId] = useState<string | undefined>(undefined);
-  const [videoUrl, setVideoUrl] = useState<string | undefined>("https://www.youtube.com/watch?v=2U9WMftV40c");
-
-  if (!videoUrl) {
-    return (
-      <div className='flex flex-col justify-center items-center'>
-      <input
-      type="text"
-      placeholder="Enter Video URL"
-      value={videoUrl || ''}
-      onChange={(e) => setVideoUrl(e.target.value)}
-    />
-    <p>No video url provided</p>
-    </div>
-    )
-  }
+  const [videoUrl, setVideoUrl] = useState<string | undefined>(
+    "https://www.youtube.com/watch?v=2U9WMftV40c",
+  );
 
   useEffect(() => {
     // parse video_url to get videoId
     if (videoUrl) {
       const url = new URL(videoUrl);
-      const videoId = url.searchParams.get('v');
+      const videoId = url.searchParams.get("v");
       if (videoId) {
         setVideoId(videoId);
       }
     }
   }, [videoUrl]);
 
-  // https://www.youtube.com/watch?v=7lPnLK9z1fI
-  const onPlayerReady: YouTubeProps['onReady'] = (event) => {
-    // access to player in all event handlers via event.target
-    event.target.pauseVideo();
+  if (!videoUrl) {
+    return (
+      <div className="flex flex-col justify-center items-center">
+        <input
+          type="text"
+          placeholder="Enter Video URL"
+          value={videoUrl || ""}
+          onChange={(e) => setVideoUrl(e.target.value)}
+        />
+        <p>No video url provided</p>
+      </div>
+    );
   }
 
-  const opts: YouTubeProps['opts'] = {
-    height: '390',
-    width: '640',
+  // https://www.youtube.com/watch?v=7lPnLK9z1fI
+  const onPlayerReady: YouTubeProps["onReady"] = (event) => {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  };
+
+  const opts: YouTubeProps["opts"] = {
+    height: "390",
+    width: "640",
     playerVars: {
       // https://developers.google.com/youtube/player_parameters
       autoplay: 1,
@@ -51,16 +53,16 @@ const  VideoViewer = ({studyId}: VideoViewerProps) => {
   };
 
   return (
-  <>
-  <input
-    type="text"
-    placeholder="Enter Video URL"
-    value={videoUrl || ''}
-    onChange={(e) => setVideoId(e.target.value)}
-  />
-  <YouTube videoId={videoId} opts={opts} onReady={onPlayerReady} />
-  
-  </>);
-}
+    <>
+      <input
+        type="text"
+        placeholder="Enter Video URL"
+        value={videoUrl || ""}
+        onChange={(e) => setVideoId(e.target.value)}
+      />
+      <YouTube videoId={videoId} opts={opts} onReady={onPlayerReady} />
+    </>
+  );
+};
 
 export default VideoViewer;
