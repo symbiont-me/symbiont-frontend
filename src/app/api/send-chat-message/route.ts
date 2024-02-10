25aebd20c09e26ff615a7b10f7cbd24fe85b1f58
lonehegelian <thelonehegelian@protonmail.com>
2024-02-10 22:49:29 +0000

    fix previous message and role issue in the prompt

--- src/app/api/send-chat-message/route.ts
+++ src/app/api/send-chat-message/route.ts
@@ -33,7 +33,6 @@ export async function POST(req: NextRequest) {
     const chatId = requestBody.chatId;
     const messages = requestBody.messages;
     const resourceIdentifier = requestBody.resourceIdentifier;
-    console.log("chatId", chatId);
     if (isNaN(chatId)) {
       return NextResponse.json({ error: "Invalid chatId" }, { status: 400 });
     }
@@ -49,20 +48,19 @@ export async function POST(req: NextRequest) {
     const fileKey = resourceIdentifier;
     // get context from pinecone
     const context = await getContext(lastMessage, fileKey);
+    console.log("previousMessage: ", messages);
+
     // create base prompt
     const prompt = {
       role: ChatCompletionRequestMessageRoleEnum.System,
-      content: `AI assistant is a brand new, powerful, human-like artificial intelligence.
+      content: `
       The traits of AI include expert knowledge, helpfulness, cleverness, and articulateness.
-      AI is a well-behaved and well-mannered individual.
-      AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user.
       AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation.
+      AI assistant will take into account any DOCUMENT BLOCK that is provided in a conversation.
       START DOCUMENT BLOCK
       ${context}
       END OF DOCUMENT BLOCK
-      AI assistant will take into account any DOCUMENT BLOCK that is provided in a conversation.
       If the context does not provide the answer to question, the AI assistant will say, "I'm sorry, but I don't know the answer to that question".
-      AI assistant will not apologize for previous responses, but instead will indicated new information was gained.
       AI assistant will not invent anything that is not drawn directly from the context.
       AI will return the response in valid Markdown format.
       `,
@@ -70,12 +68,12 @@ export async function POST(req: NextRequest) {
 
     // TODO should use md syntax so that we can parse it to display it in the frontend
     const response = await openai.createChatCompletion({
-      model: TextModels.GPT_3_5_TURBO,
+      model: TextModels.GPT_4,
       messages: [
         prompt,
         {
-          ...messages[messages.length - 1],
-          role: ChatCompletionRequestMessageRoleEnum.System,
+          content: lastMessage,
+          role: ChatCompletionRequestMessageRoleEnum.User,
         },
       ],
       stream: true,
