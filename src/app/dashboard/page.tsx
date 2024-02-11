f35bb19d2b9d8fa5ffcfa6b12d55173954971610
lonehegelian <thelonehegelian@protonmail.com>
2024-02-11 01:27:26 +0000

    test markdown

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,12 @@
+"use client";
+import Markdown from "marked-react";
 
+const test = `# Test \n 
+1. some text \n 2. some text \n 3. some text`;
 export default function Dashboard() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Welcome to the dashboard</p>
-    </div>
+    <>
+      <Markdown gfm={true} breaks={true} value={test} />
+    </>
   );
 }
