b39cddc9085da559f5206538f2937dee387c5c65
lonehegelian <thelonehegelian@protonmail.com>
2024-02-11 23:38:53 +0000

    add TODO

--- src/app/api/get-chat/route.ts
+++ src/app/api/get-chat/route.ts
@@ -1,9 +1,18 @@
-import { NextRequest, NextResponse } from 'next/server';
-import {chats} from "@/lib/db/schema";
-import {db} from "@/lib/db";
-import {eq} from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+import { chats } from "@/lib/db/schema";
+import { db } from "@/lib/db";
+import { eq } from "drizzle-orm";
+
+type GetChatRequestBody = {
+  studyId: number;
+  userId: string;
+};
+
 export async function POST(req: NextRequest, res: NextResponse) {
-    const requestBody = await req.json();
-    const _chat = await db.select().from(chats).where(eq(chats.studyId, requestBody.studyId));
-    return NextResponse.json({ chat_id: _chat[0].id }, { status: 200 });
+  const { studyId, userId }: GetChatRequestBody = await req.json();
+
+  // TODO important security check:  if the user is a member of the study
+
+  const _chat = await db.select().from(chats).where(eq(chats.studyId, studyId));
+  return NextResponse.json({ chat_id: _chat[0].id }, { status: 200 });
 }
