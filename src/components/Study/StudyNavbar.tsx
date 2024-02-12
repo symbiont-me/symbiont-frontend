4b4941a420027ac1978c2057b138f2bfe7fb3f75
lonehegelian <thelonehegelian@protonmail.com>
2024-02-12 22:39:51 +0000

    update study navbar styles

--- src/components/Study/StudyNavbar.tsx
+++ src/components/Study/StudyNavbar.tsx
@@ -1,13 +1,15 @@
 import { ViewSelected } from "@/const";
-import ProjectInfo from "./ProjectInfo";
+import StudyInfo from "./StudyInfo";
+
 type NavigationProps = {
+  studyId: string;
   setViewSelected: (view: ViewSelected) => void;
 };
 
-const Navigation = ({ setViewSelected }: NavigationProps) => {
+const Navigation = ({ setViewSelected, studyId }: NavigationProps) => {
   return (
     <>
-      <ProjectInfo />
+      <StudyInfo studyId={studyId} />
       <nav className="flex justify-between items-center p-4 bg-symbiont-900 rounded-2xl mb-2 mt-2">
         <div className="flex gap-4">
           {Object.values(ViewSelected).map((view) => (
