ca84076194a667c383e9ecc81cf7493d24e654fa
lonehegelian <thelonehegelian@protonmail.com>
2024-02-12 16:38:23 +0000

    add project name and description bar

--- src/components/Study/StudyNavbar.tsx
+++ src/components/Study/StudyNavbar.tsx
@@ -1,23 +1,25 @@
-import React from "react";
 import { ViewSelected } from "@/const";
-
+import ProjectInfo from "./ProjectInfo";
 type NavigationProps = {
   setViewSelected: (view: ViewSelected) => void;
 };
 
 const Navigation = ({ setViewSelected }: NavigationProps) => {
   return (
-    <nav className="flex justify-between items-center p-4 bg-symbiont-900 rounded-2xl mb-2 mt-2">
-      <div className="flex gap-4">
-        {Object.values(ViewSelected).map((view) => (
-          <button key={view} onClick={() => setViewSelected(view)}>
-            <p className="capitalize text-xs text-symbiont-textUnSelected">
-              {view}
-            </p>
-          </button>
-        ))}
-      </div>
-    </nav>
+    <>
+      <ProjectInfo />
+      <nav className="flex justify-between items-center p-4 bg-symbiont-900 rounded-2xl mb-2 mt-2">
+        <div className="flex gap-4">
+          {Object.values(ViewSelected).map((view) => (
+            <button key={view} onClick={() => setViewSelected(view)}>
+              <p className="capitalize text-xs text-symbiont-textUnSelected">
+                {view}
+              </p>
+            </button>
+          ))}
+        </div>
+      </nav>
+    </>
   );
 };
 
