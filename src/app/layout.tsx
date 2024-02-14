7f0261e0feecbeeccee5b931bccff366be7cd527
lonehegelian <thelonehegelian@protonmail.com>
2024-02-14 04:36:14 +0000

    use jakarta sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import ReactQueryProvider from "../components/Providers";
 import { AuthContextProvider } from "./context/AuthContext";
 
 const inter = Inter({ subsets: ["latin"] });
-
+const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Symbiont",
   description: "Symbiont is a platform to help researchers and writers",
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <ReactQueryProvider>
-        <body className={inter.className}>
+        <body className={plusJakartaSans.className}>
           <AuthContextProvider>{children}</AuthContextProvider>
         </body>
       </ReactQueryProvider>
