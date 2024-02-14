7c5d9434d4f9152a7c9608fd78e12e38e56979fc
lonehegelian <thelonehegelian@protonmail.com>
2024-02-14 02:19:34 +0000

    Add studies list to LeftSideBarMain component

--- src/components/LeftSideBar/LeftSideBarMain.tsx
+++ src/components/LeftSideBar/LeftSideBarMain.tsx
@@ -1,6 +1,45 @@
 import "@/components/LeftSideBar/leftSideBar.css";
 import OverLayGradient from "@/components/ui/OverLayGradient";
+import { useQuery } from "@tanstack/react-query";
+import { UserAuth } from "@/app/context/AuthContext";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Study } from "@/types";
+import Link from "next/link";
+import NavItem from "./NavItem";
+import { faHouse, faBook } from "@fortawesome/free-solid-svg-icons";
+
 const LeftSideBarMain = () => {
+  const authContext = UserAuth();
+  const userId = authContext?.user?.uid;
+  const [studies, setStudies] = useState<Study[]>([]);
+  if (userId) {
+    const fetchLinkedChatQuery = useQuery({
+      queryKey: ["get-studies", userId],
+      queryFn: async () => {
+        const response = await axios.post("/api/get-studies", {
+          userId: userId,
+        });
+        return response.data;
+      },
+    });
+
+    useEffect(() => {
+      if (fetchLinkedChatQuery.data) {
+        setStudies(fetchLinkedChatQuery.data);
+        console.log("fetchLinkedChatQuery.data", fetchLinkedChatQuery.data);
+      }
+    }, [fetchLinkedChatQuery.data]);
+
+    if (fetchLinkedChatQuery.isError) {
+      console.error("Error fetching chat:", fetchLinkedChatQuery.error);
+    }
+
+    if (fetchLinkedChatQuery.isLoading) {
+      return <div>Loading...</div>;
+    }
+  }
+
   return (
     <div className="left-sidebar-container h-screen bg-symbiont-foreground m-2 rounded-2xl ">
       <div className="mt-10 logo border-b-4 border-symbiont-800 flex flex-col items-center">
@@ -10,19 +49,29 @@ const LeftSideBarMain = () => {
       </div>
       <div className="navigation border-b-4 border-symbiont-800 flex">
         <div className="flex flex-col p-6">
-          <div className=" flex flex-row justify-center items-center mb-4">
-            <div className="h-4 w-4 rounded-full bg-blue-600 mr-2"></div>
-            <p className="text-sm">Home</p>
-          </div>
-
-          <div className=" flex flex-row justify-center items-center">
-            <div className="h-4 w-4 rounded-full bg-blue-600 mr-2"></div>
-            <p className="text-sm">Library</p>
-          </div>
+          <NavItem icon={faHouse} text="Home" iconColorClass="#363A3D" />
+          <NavItem icon={faBook} text="Library" iconColorClass="#363A3D" />
         </div>
       </div>
 
-      <div className="projects-list border-symbiont-800">Projects List</div>
+      <div className="projects-list border-symbiont-800">
+        <h3 className="text-xs text-symbiont-textUnSelected uppercase m-4">
+          Projects
+        </h3>
+        {studies.map((study) => (
+          <div
+            key={study.id}
+            className="flex flex-row justify-between items-center p-4 cursor-pointer"
+          >
+            <Link
+              href={`studies/${study.id}`}
+              className="text-xs ml-2 capitalize"
+            >
+              {study.name}
+            </Link>
+          </div>
+        ))}
+      </div>
       <div className="settings p-2 flex flex-col flex-end">
         <OverLayGradient />
       </div>
