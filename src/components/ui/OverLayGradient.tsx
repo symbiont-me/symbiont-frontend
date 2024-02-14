f814df6cdc3daadac4df051cd9dd39dfa6a90b84
lonehegelian <thelonehegelian@protonmail.com>
2024-02-14 01:34:01 +0000

    Refactor OverLayGradient component and add user info and settings icon

--- src/components/ui/OverLayGradient.tsx
+++ src/components/ui/OverLayGradient.tsx
@@ -1,4 +1,10 @@
 import "../ui/uiStyles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGear } from "@fortawesome/free-solid-svg-icons";
+import { UserAuth } from "@/app/context/AuthContext";
+
+// TODO modify this component doesn't need to be this complicated
+// TODO add onClick to handle LLM settings
 type OverLayGradientProps = {
   height?: string;
   width?: string;
@@ -9,7 +15,30 @@ const OverLayGradient = ({
 }: OverLayGradientProps) => {
   const tailwind = `h-${height} w-${width}`;
 
-  return <div className={`${tailwind} button-gradient rounded-2xl`}></div>;
+  const authContext = UserAuth();
+
+  return (
+    <div
+      className={`${tailwind} button-gradient rounded-2xl flex flex-row items-center justify-between`}
+    >
+      <div className="ml-2 flex flex-row items-center h-2">
+        {authContext?.user && (
+          <img
+            src={authContext.user.photoURL || ""}
+            className="rounded-full h-10 m-2"
+          />
+        )}
+        <p className="text-xs">
+          {authContext?.user ? authContext.user.displayName : "Guest"}
+        </p>
+      </div>
+      <FontAwesomeIcon
+        icon={faGear}
+        color="#686B6E"
+        className="mr-4 cursor-pointer"
+      />
+    </div>
+  );
 };
 
 export default OverLayGradient;
