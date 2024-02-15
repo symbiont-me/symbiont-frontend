c513810e357d7b78a13602df8451b7a3077d8ded
lonehegelian <thelonehegelian@protonmail.com>
2024-02-15 11:55:36 +0000

    Refactor LeftSidebar component to use new styling and fix import statements

--- src/components/LeftSidebar.tsx
+++ src/components/LeftSidebar.tsx
@@ -1,24 +1,20 @@
+import Link from "next/link";
+import { Button } from "../components/ui/Button";
+import { PlusCircle } from "lucide-react";
 
-import Link from 'next/link'
-import { Button } from '../components/ui/Button'
-import { PlusCircle } from 'lucide-react'
-
-const ChatSidebar =  () => {
-    return (
-        <div className = "flex-none max-w-xs">
-        <div className="w-full h-screen p-4 text-gray-200 bg-gray-900">
-        <Link href = "/">
-            <Button>
-                <PlusCircle className='mr-2 w-4 h-4'/>
-                Create a new chat
-            </Button>
+const ChatSidebar = () => {
+  return (
+    <div className="flex-none max-w-xs">
+      <div className="w-full h-screen p-4 text-gray-200 bg-symbiont-background">
+        <Link href="/">
+          <Button>
+            <PlusCircle className="mr-2 w-4 h-4" />
+            Create a new chat
+          </Button>
         </Link>
-      
+      </div>
+    </div>
+  );
+};
 
-
-        </div>
-        </div>
-    )
-}
-
-export default ChatSidebar
+export default ChatSidebar;
