51aa3c6a3f76b7fbb262ce8ce5cf3aeaffcd85f2
lonehegelian <thelonehegelian@protonmail.com>
2024-02-28 18:22:20 +0000

    Refactor StudyPage component and remove unused state variables

--- src/app/studies/[studyId]/page.tsx
+++ src/app/studies/[studyId]/page.tsx
@@ -27,11 +27,11 @@ import "@/app/globals.css";
 
 // an object that maps each ViewSelected enum value to a corresponding React component.
 // this allows the application to dynamically render different components based on the current view selection
+// TODO only pass Study to the components down the tree, studyId is available in the pathname
 const viewComponents: Record<
   ViewSelected,
   React.ComponentType<{
     textWriterValue: string;
-    video_url: string | undefined;
     studyId: string;
     study: Study;
   }>
@@ -51,10 +51,12 @@ export default function StudyPage() {
   const authContext = UserAuth();
   const router = useRouter();
 
-  const [viewSelected, setViewSelected] = useState<ViewSelected>(ViewSelected.Writer);
+  const [viewSelected, setViewSelected] = useState<ViewSelected>(
+    ViewSelected.Writer
+  );
   const [user, setUser] = useState<User | null>(null);
   // TODO fix
-  // NOTE note correct: authContext is undefined on first render and then it is set to the user object
+  // NOTE fix: authContext is undefined on first render and then it is set to the user object
   // and this pushes the user to the home page
   useEffect(() => {
     if (authContext?.user) {
@@ -66,61 +68,33 @@ export default function StudyPage() {
 
   // TODO Test and remove the following unused state variables
   const [textWriterValue, setTextWriterValue] = useState<string>("");
-  const [video_url, setVideoUrl] = useState<string | undefined>(undefined);
   const [currentStudy, setCurrentStudy] = useState<any>(null); // TODO set to type study
   const path = usePathname();
   const studyId = path.split("/")[2];
-  // const chatId = path.split("/")[3]; // NOTE this method can avoid a db call
-  const [chatId, setChatId] = useState<number | undefined>(undefined);
-
-  // const fetchLinkedChatQuery = useQuery({
-  //   queryKey: ["get-chat", studyId],
-  //   queryFn: async () => {
-  //     const response = await axios.post("/api/get-chat", {
-  //       studyId: studyId,
-  //       userId: user?.uid,
-  //     });
-  //     return response.data.chat_id;
-  //   },
-  // });
 
   // TODO make this a GET request
   const fetchStudyQuery = useQuery({
     queryKey: ["get-study", studyId],
     queryFn: async () => {
-      const response = await axios.post(`http://127.0.0.1:8000/get-study/?studyId=${studyId}`, {
-        studyId: studyId,
-      });
+      const response = await axios.post(
+        `http://127.0.0.1:8000/get-study/?studyId=${studyId}`
+      );
+      console.log("Study", response.data);
       return response.data;
     },
   });
 
   useEffect(() => {
     if (fetchStudyQuery.data) {
-      setCurrentStudy(fetchStudyQuery.data.study[0]);
-      // setTextWriterValue(fetchStudyQuery.data.text);
-      // setVideoUrl(fetchStudyQuery.data.video_url);
+      console.log("Study", fetchStudyQuery.data.study);
+      setCurrentStudy(fetchStudyQuery.data.study);
     }
-  }, [fetchStudyQuery.data]);
-
-  // useEffect(() => {
-  //   if (fetchLinkedChatQuery.data) {
-  //     setChatId(fetchLinkedChatQuery.data);
-  //   }
-  // }, [fetchLinkedChatQuery.data]);
-
-  // if (fetchLinkedChatQuery.isLoading) {
-  //   return (
-  //     <div className="flex flex-col items-center justify-center h-screen">
-  //       <span className="loading loading-ring loading-lg"></span>
-  //     </div>
-  //   );
-  // }
-
-  // if (fetchLinkedChatQuery.isError) {
-  //   console.error("Error fetching chat:", fetchLinkedChatQuery.error);
-  //   // TODO: handle error with a toast
-  // }
+    if (currentStudy) {
+      setTextWriterValue(currentStudy.text);
+      // This block filters the resources related to the current study to find only those of type 'video'.
+      // It then extracts the URLs of these video resources and updates the state to hold these video URLs.
+    }
+  }, [fetchStudyQuery.data, currentStudy]);
 
   const SelectedViewComponent = viewComponents[viewSelected] || null;
 
@@ -132,20 +106,22 @@ export default function StudyPage() {
       <div className="main-window">
         <div className="viewer-container">
           <div className="header">
-            <StudyNavbar setViewSelected={setViewSelected} study={currentStudy} />
+            <StudyNavbar
+              setViewSelected={setViewSelected}
+              study={currentStudy}
+            />
           </div>
           <div className="viewer">
             {SelectedViewComponent && (
               <SelectedViewComponent
                 textWriterValue={textWriterValue}
-                video_url={video_url}
                 studyId={studyId}
                 study={currentStudy}
               />
             )}
           </div>
           <div className="chat flex flex-col h-screen">
-            <ChatComponent chatId={chatId} studyId={studyId} />
+            <ChatComponent studyId={studyId} />
           </div>
         </div>
       </div>
