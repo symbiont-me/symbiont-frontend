868af84d43b2fb5d4a085346dd818ac04ce09ee7
lonehegelian <thelonehegelian@protonmail.com>
2024-02-28 18:23:17 +0000

    Refactor VideoViewer component to get Study and Video Links as array

--- src/components/Study/VideoViewer.tsx
+++ src/components/Study/VideoViewer.tsx
@@ -1,46 +1,79 @@
 import { useState, useEffect } from "react";
 import YouTube, { YouTubeProps } from "react-youtube";
+import { Study } from "@/types";
 
 type VideoViewerProps = {
-  studyId: string | undefined;
+  study: Study;
 };
 
-// TODO get videos from the studyId and display them
-// TODO Keep the video playing in the background
-const VideoViewer = ({ studyId }: VideoViewerProps) => {
-  const [videoId, setVideoId] = useState<string | undefined>(undefined);
-  const [videoUrl, setVideoUrl] = useState<string | undefined>(
-    "https://www.youtube.com/watch?v=2U9WMftV40c",
+type Resource = {
+  category: string;
+  identifier: string;
+  name: string;
+  studyId: string;
+  url: string;
+};
+
+const VideoViewer = ({ study }: VideoViewerProps) => {
+  const [currentVideoId, setCurrentVideoId] = useState<string | undefined>(
+    undefined
   );
+  const [videos, setVideos] = useState<Resource[] | null>(null);
+  const [videoIndex, setVideoIndex] = useState<number>(0);
+
+  function filterVideos() {
+    if (study.resources) {
+      const filteredVideos = study.resources.filter(
+        (resource) => resource.category === "video"
+      );
+      console.log("filteredVideos", filteredVideos);
+      setVideos(filteredVideos);
+    }
+  }
 
   useEffect(() => {
-    // parse video_url to get videoId
-    if (videoUrl) {
-      const url = new URL(videoUrl);
-      const videoId = url.searchParams.get("v");
-      if (videoId) {
-        setVideoId(videoId);
+    filterVideos();
+  }, [study]);
+
+  function createVideoId(url: string) {
+    const videoId = new URL(url).searchParams.get("v");
+    if (videoId) {
+      setCurrentVideoId(videoId);
+    }
+  }
+  useEffect(() => {
+    if (videos && videos.length > 0) {
+      createVideoId(videos[videoIndex].url);
+    }
+  }, [videos, videoIndex]);
+
+  const handleNextVideo = () => {
+    if (videos && videoIndex < videos.length - 1) {
+      const newIndex = videoIndex + 1;
+      setVideoIndex(newIndex);
+      createVideoId(videos[newIndex].url);
+    }
+  };
+
+  const handlePreviousVideo = () => {
+    if (videos && videoIndex < videos.length - 1) {
+      const newIndex = videoIndex + 1;
+      setVideoIndex(newIndex);
+      if (videos) {
+        createVideoId(videos[newIndex].url);
       }
     }
-  }, [videoUrl]);
+  };
 
-  if (!videoUrl) {
+  if (!videos || videos.length === 0) {
     return (
       <div className="flex flex-col justify-center items-center">
-        <input
-          type="text"
-          placeholder="Enter Video URL"
-          value={videoUrl || ""}
-          onChange={(e) => setVideoUrl(e.target.value)}
-        />
         <p>No video url provided</p>
       </div>
     );
   }
 
-  // https://www.youtube.com/watch?v=7lPnLK9z1fI
   const onPlayerReady: YouTubeProps["onReady"] = (event) => {
-    // access to player in all event handlers via event.target
     event.target.pauseVideo();
   };
 
@@ -48,20 +81,17 @@ const VideoViewer = ({ studyId }: VideoViewerProps) => {
     height: "390",
     width: "640",
     playerVars: {
-      // https://developers.google.com/youtube/player_parameters
       autoplay: 1,
     },
   };
 
   return (
     <>
-      <input
-        type="text"
-        placeholder="Enter Video URL"
-        value={videoUrl || ""}
-        onChange={(e) => setVideoId(e.target.value)}
-      />
-      <YouTube videoId={videoId} opts={opts} onReady={onPlayerReady} />
+      <div className="video-navigation">
+        <button onClick={handlePreviousVideo}>Previous</button>
+        <button onClick={handleNextVideo}>Next</button>
+      </div>
+      <YouTube videoId={currentVideoId} opts={opts} onReady={onPlayerReady} />
     </>
   );
 };
