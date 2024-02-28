09f300469df09ef481ffa72b22e6c585226c86b6
lonehegelian <thelonehegelian@protonmail.com>
2024-02-28 18:22:43 +0000

    Update StudyInfo component to display study description

--- src/components/Study/StudyInfo.tsx
+++ src/components/Study/StudyInfo.tsx
@@ -7,12 +7,13 @@ const StudyInfo = ({ study }: { study: Study }) => {
   const placeholder =
     "https://images.unsplash.com/photo-1455390582262-044cdead277a";
 
-  console.log(study);
   return (
     <div className="w-full h-20 bg-symbiont-foreground mt-2 mb-2 p-2 pl-4  rounded-2xl flex flex-row justify-between">
       <div className="flex flex-col items-start space-y-2">
         <h1 className="text-sm font-bold">{study?.name}</h1>
-        <p className="text-symbiont-textUnSelected text-xs ">Description</p>
+        <p className="text-symbiont-textUnSelected text-xs ">
+          {study?.description}
+        </p>
         <div className="flex flex-row justify-center items-center">
           <p className="text-2xs pr-2"> Last Saved: 18:32</p>
         </div>
