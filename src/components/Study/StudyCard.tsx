e624e7e868d88360e708ae9dd269dd9efa2c4768
lonehegelian <thelonehegelian@protonmail.com>
2024-03-08 11:21:59 +0000

    Add study deletion functionality to StudyCard component

--- src/components/Study/StudyCard.tsx
+++ src/components/Study/StudyCard.tsx
@@ -1,12 +1,55 @@
+// TODO The dashboard component should rerender when a study is deleted
 import { Study } from "../../types";
 import Link from "next/link";
 import "./studyStyles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import { UserAuth } from "@/app/context/AuthContext";
+import { useState, useEffect } from "react";
+
+async function deleteStudy(studyId: string, userToken: string) {
+  const url = `http://127.0.0.1:8000/delete-study?studyId=${studyId}`;
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${userToken}`,
+  };
+  try {
+    await axios.delete(url, { headers });
+  } catch (error) {
+    console.error("Error deleting study:", error);
+  }
+}
 
 const StudyCard = ({ study }: { study: Study }) => {
-  console.log(study);
+  const authContext = UserAuth();
+  const [userToken, setUserToken] = useState<string>("");
 
+  if (!authContext) {
+    return null;
+  }
+
+  useEffect(() => {
+    const getUserAuthToken = async () => {
+      if (authContext?.user?.getIdToken) {
+        const token = await authContext.user.getIdToken();
+        setUserToken(token);
+      }
+    };
+    getUserAuthToken();
+  });
+
+  const handleDelete = () => {
+    if (study.id && userToken) {
+      deleteStudy(study.id.toString(), userToken);
+    }
+  };
   return (
     <div className="container bg-symbiont-background w-60 m-4 h-60 rounded-xl p-0 border border-gray-500">
+      <div className="cursor-pointer" onClick={handleDelete}>
+        {" "}
+        <FontAwesomeIcon icon={faTrash} />{" "}
+      </div>
       <div className="card-title">
         <Link href={`studies/${study.id}`} className="">
           <h1 className="text-sm p-4">{study.name}</h1>
