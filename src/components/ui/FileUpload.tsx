c1328c8901353d746aec82faff2e09a84d1cfdcf
lonehegelian <thelonehegelian@protonmail.com>
2024-03-09 16:58:07 +0000

    remove useMutation hook from the the FileUpload component

--- src/components/ui/FileUpload.tsx
+++ src/components/ui/FileUpload.tsx
@@ -7,6 +7,9 @@ import ToastMessage from "./ToastMessage";
 import { usePathname } from "next/navigation";
 import { StudyResource, StudyResourceCategory } from "@/types";
 import { uploadToFirebaseStorage } from "@/firebase/uploadToStorage";
+import { UserAuth } from "@/app/context/AuthContext";
+import { useEffect } from "react";
+
 // TODO fix toast messages
 // TODO update to handle audio file uploads
 
@@ -18,18 +21,22 @@ type FileUploadData = {
   category: string;
 };
 
-async function sendFileUploadRequest(file: File): Promise<FileUploadData> {
+async function sendFileUploadRequest(
+  file: File,
+  studyId: string,
+  userToken: string
+): Promise<FileUploadData> {
+  const endpoint = `http://127.0.0.1:8000/upload-resource?studyId=${studyId}`;
   const formData = new FormData();
   formData.append("file", file);
-  const response = await axios.post(
-    `http://127.0.0.1:8000/upload-resource/?studyId=Pp0ZYO6EL54A2XiIr7Eu`,
-    formData,
-    {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    }
-  );
+  const body = formData;
+
+  console.log(userToken);
+  const headers = {
+    "Content-Type": "multipart/form-data",
+    Authorization: `Bearer ${userToken}`,
+  };
+  const response = await axios.post(endpoint, body, { headers });
   return response.data.resource;
 }
 
@@ -39,40 +46,25 @@ const FileUpload = () => {
   const studyId = path.split("/")[2];
   // TODO use uploading state
   const [uploading, setUploading] = useState(false);
+  const authContext = UserAuth();
+  const [userToken, setUserToken] = useState<string | undefined>(undefined);
+  const [loading, setLoading] = useState(true);
 
-  // useMutation is a hook from react-query that handles asynchronous updates
-  const fileUploadRequest = useMutation({
-    // The mutate method is called within the onDrop method of the useDropzone hook, where data returned from uploadToFirebaseStorage is passed to it. This data object should contain the fileKey, fileName, and downloadUrl needed to construct the resourceData object.
-    mutationFn: async ({ identifier, name, url }: FileUploadData) => {
-      if (!identifier || !name || !url) {
-        throw new Error("fileKey or fileName is undefined");
-      }
-      try {
-        // TODO set file type either audio or pdf
-        const fileType = "pdf" as StudyResourceCategory;
-        // create the resource in the database
-        const resourceData: StudyResource = {
-          studyId: parseInt(studyId),
-          name: name,
-          url: url,
-          identifier: identifier,
-          category: fileType,
-        };
+  async function getUserAuthToken() {
+    if (authContext?.user?.getIdToken) {
+      const token = await authContext.user.getIdToken();
+      setUserToken(token);
+    }
+  }
 
-        const response = await axios.post(
-          `http://127.0.0.1:8000/upload-resource?studyId=${studyId}`
-        );
-        // TODO use the HTTP status code defined in Types
-        if (response.status !== 201) {
-          <ToastMessage message="Error creating resource" type="error" />;
-          throw new Error("Error creating resource");
-        }
-        return;
-      } catch (e) {
-        throw e;
-      }
-    },
-  });
+  useEffect(() => {
+    const fetchToken = async () => {
+      await getUserAuthToken();
+      setLoading(false);
+    };
+    fetchToken();
+  }, []);
+
   // useDropzone is a hook that manages file dropping functionality
   const { getRootProps, getInputProps } = useDropzone({
     // TODO accept and handle PDF and audio files
@@ -90,48 +82,35 @@ const FileUpload = () => {
       }
       try {
         setUploading(true);
-        // send the file to the FastAPI server
-        const data = await sendFileUploadRequest(file);
-        console.log(data);
-
-        if (!data?.identifier || !data.name) {
-          return;
-        }
-        const { identifier, name, url, category, study_id } = data;
-        // The mutate function is used to asynchronously upload a file and create a resource in the database, with onSuccess and onError callbacks for post-upload handling.
-        fileUploadRequest.mutate(
-          { identifier, name, url, study_id, category }, // Adjust "pdf" as necessary
-          {
-            onSuccess: () => {
-              /*  NOTE this is supposed to referesh the resources query in the ResourceSwitcher component
-                but seems to be working without it
-            */
-              // console.log("successfully uploaded file to s3 and created resource. Now invalidating resources query.");
-              // queryClient.invalidateQueries({ queryKey: ['resources', studyId] })
-            },
-            onError: (err) => {
-              <ToastMessage message="Error creating chat" type="error" />;
-              console.error(err);
-            },
+        if (userToken) {
+          const uploadResponse = await sendFileUploadRequest(
+            file,
+            studyId,
+            userToken
+          );
+          if (uploadResponse) {
+            setUploading(false);
           }
-        );
+        }
       } catch (error) {
         console.log(error);
-      } finally {
-        setUploading(false);
       }
     },
   });
   // Render the dropzone UI
   return (
     <div className="flex justify-center items-start mr-4 mt-2">
-      <div {...getRootProps({ className: "dropzone" })}>
-        <input {...getInputProps()} />
-        <input
-          type="file"
-          className="file-input file-input-bordered file-input-primary w-full max-w-xs"
-        />
-      </div>
+      {uploading ? (
+        <span className="loading loading-dots loading-sm"></span>
+      ) : (
+        <div {...getRootProps({ className: "dropzone" })}>
+          <input {...getInputProps()} />
+          <input
+            type="file"
+            className="file-input file-input-bordered file-input-primary w-full max-w-xs"
+          />
+        </div>
+      )}
     </div>
   );
 };
