f287ed31d0e7a72bcb21188ed1bf7c54146c495f
lonehegelian <thelonehegelian@protonmail.com>
2024-03-09 19:54:51 +0000

    Commented out unused code in useFileUpload hook

--- src/hooks/useFileUpload.tsx
+++ src/hooks/useFileUpload.tsx
@@ -1,47 +1,51 @@
-import { useMutation } from "@tanstack/react-query";
-import axios from "axios";
-import { UserAuth } from "@/app/context/AuthContext";
-import { usePathname } from "next/navigation";
+// import { useMutation } from "@tanstack/react-query";
+// import axios from "axios";
+// import { UserAuth } from "@/app/context/AuthContext";
+// import { usePathname } from "next/navigation";
 
-type FileUploadData = {
-  study_id: string;
-  identifier: string;
-  name: string;
-  url: string;
-  category: string;
-};
+// type FileUploadData = {
+//   study_id: string;
+//   identifier: string;
+//   name: string;
+//   url: string;
+//   category: string;
+// };
 
-const sendFileUpload = async (
-  file: File,
-  userToken: string,
-  studyId: string
-): Promise<FileUploadData> => {
-  const formData = new FormData();
-  formData.append("file", file);
-  const response = await axios.post(
-    `http://127.0.0.1:8000/upload-resource/?studyId=${studyId}`,
-    formData,
-    {
-      headers: {
-        "Content-Type": "multipart/form-data",
-        Authorization: `Bearer ${userToken}`,
-      },
-    }
-  );
-  return response.data.resource;
-};
+// const sendFileUpload = async (
+//   file: File,
+//   userToken: string,
+//   studyId: string
+// ): Promise<FileUploadData> => {
+//   const formData = new FormData();
+//   formData.append("file", file);
+//   const response = await axios.post(
+//     `http://127.0.0.1:8000/upload-resource/?studyId=${studyId}`,
+//     formData,
+//     {
+//       headers: {
+//         "Content-Type": "multipart/form-data",
+//         Authorization: `Bearer ${userToken}`,
+//       },
+//     }
+//   );
+//   return response.data.resource;
+// };
 
-export const useFileUpload = () => {
-  const authContext = UserAuth();
-  const studyId = usePathname().split("/")[2];
-  const userTokenPromise = authContext?.user?.getIdToken();
+// async function useFileUpload() {
+//   const authContext = UserAuth();
+//   const studyId = usePathname().split("/")[2];
+//   const userTokenPromise = authContext?.user?.getIdToken();
 
-  const mutation = useMutation({
-    mutationFn: (file: File) =>
-      userTokenPromise
-        ? userTokenPromise.then((token) => sendFileUpload(file, token, studyId))
-        : Promise.reject(new Error("No user token")),
-  });
+//   console.log("userTokenPromise", userTokenPromise);
 
-  return mutation;
-};
+//   const mutation = useMutation({
+//     mutationFn: (file: File) =>
+//       userTokenPromise
+//         ? userTokenPromise.then((token) => sendFileUpload(file, token, studyId))
+//         : Promise.reject(new Error("No user token")),
+//   });
+
+//   return mutation;
+// }
+
+// export default useFileUpload;
