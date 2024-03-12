98544a543473faf33dfad04b1618f3a75c1eac80
lonehegelian <thelonehegelian@protonmail.com>
2024-03-12 23:54:04 +0000

    Refactor AiChatMessage component to use Typography for better text styling

--- src/components/ChatComponent/AiChatMessage.tsx
+++ src/components/ChatComponent/AiChatMessage.tsx
@@ -1,9 +1,16 @@
 import Markdown from "marked-react";
+import { Typography } from "@mui/material";
 
 const AiChatMessage = ({ message }: { message: string }) => {
   return (
     <>
-      <Markdown gfm={true} breaks={true} value={message} />
+      <Typography
+        variant="body1"
+        component="p"
+        style={{ textAlign: "justify" }}
+      >
+        <Markdown gfm={true} breaks={true} value={message} />
+      </Typography>
     </>
   );
 };
