32807bbafb4959a1c7007e86183bb3697ebd3c7d
lonehegelian <thelonehegelian@protonmail.com>
2024-03-12 23:53:59 +0000

    Refactor ResourceSwitcher component to use Material-UI Select

--- src/components/ResourceSwitcher.tsx
+++ src/components/ResourceSwitcher.tsx
@@ -1,21 +1,26 @@
-// TODO move this to Study folder
-
 import { useState, useEffect } from "react";
 import { StudyResource } from "@/types";
 import { truncateFileName } from "@/lib/utils";
 import { useStudyContext } from "@/app/context/StudyContext";
+
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 
 type ResourceSwitcherProps = {
   studyId: string;
   onResourceChange: (resource: StudyResource) => void;
 };
 
-// TODO give option to "Search All"
 const ResourceSwitcher = ({
   studyId,
   onResourceChange,
 }: ResourceSwitcherProps) => {
   const [resources, setResources] = useState<StudyResource[]>([]);
+  const [selectedResource, setSelectedResource] =
+    useState<StudyResource | null>(null);
   const studyContext = useStudyContext();
 
   if (!studyContext) {
@@ -25,36 +30,44 @@ const ResourceSwitcher = ({
   useEffect(() => {
     if (studyContext?.study) {
       setResources(studyContext.study.resources);
+      // set the first resource as selected by default if needed
+      if (studyContext.study.resources.length > 0 && !selectedResource) {
+        const firstResource = studyContext.study.resources[0];
+        setSelectedResource(firstResource);
+        onResourceChange(firstResource);
+      }
     }
   }, [studyContext]);
 
-  const handleResourceChange = (
-    event: React.ChangeEvent<HTMLSelectElement>
-  ) => {
+  const handleResourceChange = (event: SelectChangeEvent<string>) => {
     const resourceIdentifier = event.target.value;
-    const resource = resources?.find(
-      (r) => r.identifier === resourceIdentifier
-    );
+    const resource = resources.find((r) => r.identifier === resourceIdentifier);
     if (resource) {
+      setSelectedResource(resource);
       onResourceChange(resource);
     }
   };
 
   return (
     <div>
-      <h2 className="text-xs mb-4 text-center font-bold">Context Switcher</h2>
-      <select
-        className="select select-bordered w-full max-w-xs"
-        onChange={handleResourceChange}
-      >
-        {resources &&
-          Array.isArray(resources) &&
-          resources.map((resource) => (
-            <option key={resource.identifier} value={resource.identifier}>
-              {truncateFileName(resource.name)}
-            </option>
-          ))}
-      </select>
+      <Box sx={{ minWidth: 120 }}>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Resource</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={selectedResource?.identifier || ""}
+            label="Resource"
+            onChange={handleResourceChange}
+          >
+            {resources.map((resource) => (
+              <MenuItem key={resource.identifier} value={resource.identifier}>
+                {truncateFileName(resource.name)}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
     </div>
   );
 };
