94f7b1645c2deb9170058f88a6b1dcafa7b74d9d
lonehegelian <thelonehegelian@protonmail.com>
2024-03-15 23:40:33 +0000

    Refactor StudyInfo component layout and typography

--- src/components/Study/StudyInfo.tsx
+++ src/components/Study/StudyInfo.tsx
@@ -1,26 +1,32 @@
 import { Study } from "@/types";
 import { faFloppyDisk } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Typography } from "@mui/material";
+import Divider from "@mui/material/Divider";
+import Image from "next/image";
 
 // TODO replace time with actual time
+
 const StudyInfo = ({ study }: { study: Study }) => {
   const placeholder =
     "https://images.unsplash.com/photo-1455390582262-044cdead277a";
 
   return (
-    <div className="w-full h-20 bg-symbiont-foreground mt-2 mb-2 p-2 pl-4  rounded-2xl flex flex-row justify-between">
+    <div className="w-full h-20  mt-2 mb-10 p-2 pl-4 flex flex-row justify-between">
       <div className="flex flex-col items-start space-y-2">
-        <h1 className="text-sm font-bold">{study?.name}</h1>
-        <p className="text-symbiont-textUnSelected text-xs ">
-          {study?.description}
-        </p>
+        <Typography variant="h3">{study?.name}</Typography>
+        <Typography variant="body2">{study?.description}</Typography>
         <div className="flex flex-row justify-center items-center">
-          <p className="text-2xs pr-2"> Last Saved: 18:32</p>
+          <Typography variant="caption" className="pr-2">
+            Last Saved: 18:32
+          </Typography>
         </div>
       </div>
-      <div className="flex flex-col mr-8 items-center justify-center">
-        <img
-          className="h-10 w-10 bg-symbiont-chatMessageUser rounded-full"
+      <div className="flex flex-col mr-8 items-center justify-center rounded-full">
+        <Image
+          height={80}
+          width={80}
+          alt={study?.name}
           src={study?.image ? study.image : placeholder}
         />
       </div>
