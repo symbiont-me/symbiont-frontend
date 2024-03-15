53b04a47b1970d4b7f337e7a2146b9eb6ea0859f
lonehegelian <thelonehegelian@protonmail.com>
2024-03-15 23:39:31 +0000

    Delete leftSideBar.css, add LLMModels enum, update StudyCard and StudyInfo components, and implement Summaries component

---
+++ src/components/LeftSideBar/Drawer.tsx
@@ -0,0 +1,250 @@
+import * as React from "react";
+import { styled, useTheme, Theme, CSSObject } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import HomeIcon from "@mui/icons-material/Home";
+import LibraryBooksIcon from "@mui/icons-material/LibraryBooks";
+import Image from "next/image";
+import Link from "next/link";
+import LLMSettings from "../Settings/LLMSettings";
+import Dialog from "@mui/material/Dialog";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import CloseIcon from "@mui/icons-material/Close";
+import Slide from "@mui/material/Slide";
+import { TransitionProps } from "@mui/material/transitions";
+import { Button } from "@mui/material";
+import Typography from "@mui/material/Typography";
+
+const drawerWidth = 240;
+
+function openedMixin(theme: Theme): CSSObject {
+  return {
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: "hidden",
+  };
+}
+
+function closedMixin(theme: Theme): CSSObject {
+  return {
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: "hidden",
+    width: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up("sm")]: {
+      width: `calc(${theme.spacing(8)} + 1px)`,
+    },
+  };
+}
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement;
+  },
+  ref: React.Ref<unknown>
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+type FullScreenDrawerDialogProps = {
+  drawerOpen: boolean;
+  handleDrawerClose: () => void;
+  handleSettingsOpen: () => void;
+};
+
+const Drawer = ({
+  drawerOpen,
+  handleDrawerClose,
+  handleSettingsOpen,
+}: FullScreenDrawerDialogProps) => {
+  <Box sx={{ display: "flex", flexDirection: "column", height: "100%" }}>
+    <CssBaseline />
+
+    <Drawer variant="permanent" open={drawerOpen}>
+      <DrawerHeader>
+        {drawerOpen ? (
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "rtl" ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
+          </IconButton>
+        ) : (
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+              marginRight: "4px",
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+        )}
+      </DrawerHeader>
+      <Divider />
+      <List>
+        {["Home", "Library"].map((text, index) => (
+          <ListItem key={text} disablePadding sx={{ display: "block" }}>
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: drawerOpen ? "initial" : "center",
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: drawerOpen ? 3 : "auto",
+                  justifyContent: "center",
+                }}
+              >
+                {index % 2 === 0 ? (
+                  <Link href="/">
+                    <HomeIcon />
+                  </Link>
+                ) : (
+                  <Link href="/library">
+                    <LibraryBooksIcon />
+                  </Link>
+                )}
+              </ListItemIcon>
+              <ListItemText
+                primary={text}
+                sx={{ opacity: drawerOpen ? 1 : 0 }}
+              />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List sx={{ flexGrow: 1 }}>
+        {["Chrome Extension", "Discord"].map((text, index) => (
+          <ListItem key={text} disablePadding sx={{ display: "block" }}>
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: drawerOpen ? "initial" : "center",
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: drawerOpen ? 3 : "auto",
+                  justifyContent: "center",
+                }}
+              >
+                {text === "Chrome Extension" && (
+                  <Image
+                    src="https://www.svgrepo.com/show/475640/chrome-color.svg"
+                    width={24}
+                    height={24}
+                    alt="chrome"
+                  />
+                )}
+                {text === "Discord" && (
+                  <Image
+                    src="https://www.svgrepo.com/show/353655/discord-icon.svg"
+                    width={24}
+                    height={24}
+                    alt="discord"
+                  />
+                )}
+              </ListItemIcon>
+              <ListItemText
+                primary={text}
+                sx={{ opacity: drawerOpen ? 1 : 0 }}
+              />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+
+      <List>
+        {["Settings"].map((text, index) => (
+          <ListItem key={text} disablePadding sx={{ display: "block" }}>
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: drawerOpen ? "initial" : "center",
+                px: 2.5,
+              }}
+              onClick={handleSettingsOpen}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: drawerOpen ? 3 : "auto",
+                  justifyContent: "center",
+                }}
+              >
+                {text === "Settings" && (
+                  <Image
+                    src="https://www.svgrepo.com/show/491639/gear.svg"
+                    width={24}
+                    height={24}
+                    alt="settings"
+                  />
+                )}
+              </ListItemIcon>
+              <ListItemText
+                primary={text}
+                sx={{ opacity: drawerOpen ? 1 : 0 }}
+              />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+  </Box>;
+};
+
+export default Drawer;
