27b3d4b15b7c8b6fa8f51da14e3fb243e476238b
lonehegelian <thelonehegelian@protonmail.com>
2024-03-15 23:39:08 +0000

    Add LLMSettingsPage component

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,13 @@
+import LLMSettings from "@/components/Settings/LLMSettings";
+
+const LLMSettingsPage = () => {
+  return (
+    <div>
+      <h1>
+        <LLMSettings />
+      </h1>
+    </div>
+  );
+};
+
+export default LLMSettingsPage;
