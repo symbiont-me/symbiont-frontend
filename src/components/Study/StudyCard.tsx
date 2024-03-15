87682e77d79cb86a4aefc6f3375b6ce692c350b3
lonehegelian <thelonehegelian@protonmail.com>
2024-03-15 23:40:27 +0000

    Remove commented out code in StudyCard component

--- src/components/Study/StudyCard.tsx
+++ src/components/Study/StudyCard.tsx
@@ -4,38 +4,6 @@ import "./studyStyles.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { useStudyContext } from "@/app/context/StudyContext";
-
-// const StudyCard = ({ study }: { study: Study }) => {
-//   const studyContext = useStudyContext();
-
-//   const handleDelete = () => {
-//     if (studyContext && study.id) {
-//       studyContext.deleteStudy(study.id.toString());
-//     }
-//   };
-//   return (
-//     <div className="container bg-symbiont-background w-60 m-4 h-60 rounded-xl p-0 border border-gray-500">
-//       <div className="cursor-pointer" onClick={handleDelete}>
-//         {" "}
-//         <FontAwesomeIcon icon={faTrash} />{" "}
-//       </div>
-//       <div className="card-title">
-//         <Link href={`studies/${study.id}`} className="">
-//           <h1 className="text-sm p-4">{study.name}</h1>
-//         </Link>
-//       </div>
-//       <div className="card-image p-4">
-//         <img className="h-20 w-20 object-cover rounded-xl" src={study.image} />
-//       </div>
-//       <div className="card-description p-4">
-//         <p className="text-xs">{study.description}</p>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default StudyCard;
-
 import * as React from "react";
 import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
