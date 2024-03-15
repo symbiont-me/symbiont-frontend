a4700221137feeccc6a8c0140788d798be2423d1
lonehegelian <thelonehegelian@protonmail.com>
2024-03-15 23:40:38 +0000

    Add accordion component and implement delete functionality in Summaries component

--- src/components/Study/Summaries.tsx
+++ src/components/Study/Summaries.tsx
@@ -2,6 +2,17 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { Study } from "@/types";
 import { useStudyContext } from "@/app/context/StudyContext";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Typography from "@mui/material/Typography";
+import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from "@mui/material/IconButton";
+import AccordionActions from "@mui/material/AccordionActions";
+import Button from "@mui/material/Button";
+
+// TODO Implement Delete Resource functionality
 
 const Summaries = () => {
   const studyContext = useStudyContext();
@@ -13,25 +24,44 @@ const Summaries = () => {
     }
   }, [studyContext]);
 
+  function deleteResource(resourceIdentifier: string) {
+    studyContext?.deleteResource(resourceIdentifier);
+  }
+
   return (
     <div>
-      <h1>Summaries</h1>
+      <div>
+        <Typography variant="h3">Summaries</Typography>
+      </div>
+
       {studyContext?.isStudyLoading ? (
         <div>Loading...</div>
       ) : (
         <ul>
           {study?.resources.map((resource) => (
-            <div key={resource.identifier}>
-              <li>
+            <Accordion key={resource.identifier}>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1-content"
+                id="panel1-header"
+              >
+                {/* TODO what should be the href here? only for webpages or evey resource? */}
                 <a
-                  href={resource.url}
-                  className="hover:underline text-symbiont-chatMessageUser"
+                  href={resource.url ? resource.url : "#"}
+                  className="hover:underline text-blue-500"
                 >
                   {resource.name}
                 </a>
-              </li>
-              <p>{resource.summary}</p>
-            </div>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography variant="body1">{resource.summary}</Typography>
+              </AccordionDetails>
+              <AccordionActions>
+                <Button onClick={() => deleteResource(resource.identifier)}>
+                  Delete Resource
+                </Button>
+              </AccordionActions>
+            </Accordion>
           ))}
         </ul>
       )}
