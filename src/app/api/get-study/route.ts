2af6e33a093b881a94d13b5a921d0ae2d1e7970a
lonehegelian <thelonehegelian@protonmail.com>
2024-03-16 14:34:01 +0000

    removes server api routes

--- src/app/api/get-study/route.ts
+++
@@ -1,9 +0,0 @@
-import { studies } from "@/lib/db/schema";
-import { db } from "@/lib/db";
-import { eq } from "drizzle-orm";
-import { NextRequest, NextResponse } from "next/server";
-export const POST = async (req: NextRequest, res: NextResponse) => {
-  const { studyId } = await req.json();
-  const study = await db.select().from(studies).where(eq(studies.id, studyId));
-  return NextResponse.json({ study });
-};
