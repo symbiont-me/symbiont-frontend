ab9f03959e2d9be2f04f8a7e5a8c197260cca108
lonehegelian <thelonehegelian@protonmail.com>
2024-03-17 00:26:37 +0000

    Refactor getEmbeddings function in embeddings.ts

--- src/lib/embeddings.ts
+++ src/lib/embeddings.ts
@@ -2,29 +2,22 @@ import { OpenAIApi, Configuration } from "openai-edge";
 
 const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
-})
-
+});
 
 const openai = new OpenAIApi(config);
 
-export async function getEmbeddings(text: string) : Promise<number[]>{
-  
+export async function getEmbeddings(text: string): Promise<number[]> {
   try {
-
     const response = await openai.createEmbedding({
       model: "text-embedding-ada-002",
       input: text.replace(/\n/g, " "),
-
     });
 
+    const result = await response.json();
 
-    const result = await response.json()
-    
-    return result.data[0].embedding as number[]
+    return result.data[0].embedding as number[];
   } catch (error) {
-    console.log(error)
-    throw error
+    console.log(error);
+    throw error;
   }
 }
-
-
