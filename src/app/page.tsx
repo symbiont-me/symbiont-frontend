6473193f3351492c1ef5af77ada25c0ed03f35f8
lonehegelian <thelonehegelian@protonmail.com>
2024-03-22 14:51:06 +0000

    Update page title in Home component to Symbiont

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,9 +30,7 @@ export default function Home() {
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center">
           <div className="flex items-center">
-            <h1 className="mr-3 text-5xl font-semibold">
-              Mistah Kurtz - he dead.
-            </h1>
+            <h1 className="mr-3 text-5xl font-semibold">Symbiont</h1>
           </div>
           <div className="flex mt-2"></div>
           <div className="w-full mt-4">
