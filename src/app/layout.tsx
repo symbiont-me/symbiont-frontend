4b470b1d354a54453b024a50c851e6476720f85f
lonehegelian <thelonehegelian@protonmail.com>
2024-04-05 06:54:55 +0100

    update font to lato

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,24 @@
 import type { Metadata } from "next";
-import { Inter, Plus_Jakarta_Sans } from "next/font/google";
+import {
+  Inter,
+  Plus_Jakarta_Sans,
+  Raleway,
+  Montserrat,
+  Roboto,
+  Lato,
+  Open_Sans,
+} from "next/font/google";
 import "./globals.css";
 import ReactQueryProvider from "../components/Providers";
 import { AuthContextProvider } from "./context/AuthContext";
 import { StudyProvider } from "./context/StudyContext";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ["latin"] });
+const raleway = Raleway({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ["latin"] });
+const roboto = Roboto({ subsets: ["latin"], weight: "400" });
+const lato = Lato({ subsets: ["latin"], weight: "400" });
+const open_sans = Open_Sans({ subsets: ["latin"], weight: "400" });
 export const metadata: Metadata = {
   title: "Symbiont",
   description: "Symbiont is a platform to help researchers and writers",
@@ -20,7 +33,7 @@ export default function RootLayout({
     <html lang="en">
       {/* ? shouldn't the reactqueryprovider be below the body tag ? */}
       <ReactQueryProvider>
-        <body className={plusJakartaSans.className}>
+        <body className={lato.className}>
           <AuthContextProvider>
             <StudyProvider>
               <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
