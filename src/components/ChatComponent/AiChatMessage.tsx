8168c2f8c0686a246c395ca56aa5edfc83717afe
lonehegelian <thelonehegelian@protonmail.com>
2024-04-05 07:39:18 +0100

    NOTE: add a note on styles issue and a warning

--- src/components/ChatComponent/AiChatMessage.tsx
+++ src/components/ChatComponent/AiChatMessage.tsx
@@ -5,7 +5,11 @@ const AiChatMessage = ({ message }: { message: string }) => {
   return (
     <>
       <div className="break-words leading-6 text-sm">
-        <Markdown gfm={true} breaks={true} value={message} />
+        {/* @note removing the p tag will make the copy break. If you can fix both, great, otherwise leave it be */}
+        {/* there is a minor warning about p descendant of p */}
+        <p>
+          <Markdown gfm={true} breaks={true} value={message} />
+        </p>
       </div>
     </>
   );
