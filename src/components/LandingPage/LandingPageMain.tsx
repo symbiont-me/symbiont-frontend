1fd4508e53845c2f28b000db0ede01f5ffed6cec
lonehegelian <thelonehegelian@protonmail.com>
2024-04-12 00:38:30 +0100

    add faq component to the landing page

--- src/components/LandingPage/LandingPageMain.tsx
+++ src/components/LandingPage/LandingPageMain.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Hero from "./Hero";
-
+import FAQ from "./FAQ";
 const LandingPageMain = () => {
   return (
     <div className="w-screen h-screen min-h-screen flex flex-col sm:h-full overflow-x-hidden">
       <Navbar />
       <Hero />
+      <FAQ />
     </div>
   );
 };
