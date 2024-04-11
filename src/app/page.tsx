41e072713c5a76365233830b6e49795d4db6a98d
lonehegelian <thelonehegelian@protonmail.com>
2024-04-11 03:46:58 +0100

    update authcontext loading states

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,33 +5,50 @@ import { LogIn } from "lucide-react";
 import UserDashboard from "@/components/Dashboard/UserDashboardMain";
 import { UserAuth } from "@/app/context/AuthContext";
 import LandingPage from "@/components/LandingPage/LandingPageMain";
-
+import { useEffect, useState } from "react";
+import { CircularProgress } from "@mui/material";
 export default function Home() {
   const authContext = UserAuth();
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    // Assuming `UserAuth` provides a way to check if the auth status is being resolved
+    const checkAuthStatus = async () => {
+      try {
+        if (!authContext) {
+          return;
+        }
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setLoading(false);
+      }
+    };
+
+    checkAuthStatus();
+  }, [authContext, authContext?.user, authContext?.user?.uid]);
   if (!authContext) {
-    throw new Error("AuthContext is not available");
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <CircularProgress />
+    </div>;
   }
-  const handleSignIn = () => {
-    try {
-      authContext.googleSignIn();
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  if (authContext.user && authContext.user.uid) {
+  if (loading) {
     return (
-      <div className="flex flex-col h-full">
-        <UserDashboard />
+      <div className="h-screen w-screen flex flex-col justify-center items-center">
+        <CircularProgress />
       </div>
     );
   }
 
   return (
-    // TODO Create LandingPage component
     <>
-      <LandingPage />
+      {authContext?.user ? (
+        <div className="flex flex-col h-full">
+          <UserDashboard />
+        </div>
+      ) : (
+        <LandingPage />
+      )}
     </>
   );
 }
