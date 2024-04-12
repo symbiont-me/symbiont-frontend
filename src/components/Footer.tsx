f780de275aaa6401afd5b30ef175a4362151e8ff
lonehegelian <thelonehegelian@protonmail.com>
2024-04-12 02:27:48 +0100

    feat: add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="min-h-3/4 max-h-3/4 w-full bg-slate-50 flex flex-row items-center justify-center p-8">
+      Made with ❤️ by the team at Symbiont
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/LandingPage/LandingPageMain.tsx
+++ src/components/LandingPage/LandingPageMain.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./Navbar";
 import Hero from "./Hero";
 import FAQ from "./FAQ";
+import Footer from "../Footer";
 const LandingPageMain = () => {
   return (
     <div className="w-screen h-screen min-h-screen flex flex-col sm:h-full overflow-x-hidden">
       <Navbar />
       <Hero />
       <FAQ />
+      <Footer />
     </div>
   );
 };
