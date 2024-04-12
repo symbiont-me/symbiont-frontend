7f6e1770ec6390a31febdf736c14842c7281ff35
lonehegelian <thelonehegelian@protonmail.com>
2024-04-12 17:27:23 +0100

    update FAQ text and display api links

--- src/components/LandingPage/FAQ.tsx
+++ src/components/LandingPage/FAQ.tsx
@@ -30,16 +30,16 @@ const faqs: FAQ[] = [
   {
     question: "Where can I get the API key?",
     answer:
-      "API Keys for various LLMs and models can be gotten from a provider like OpenAI, Anthropic or Google. \n OpenAI: https://platform.openai.com/api-keys \n Anthropic: https://docs.anthropic.com/claude/reference/getting-started-with-the-api",
+      "API Keys for various LLMs and models can be gotten from a provider like OpenAI, Anthropic or Google.",
   },
   {
     question: "What will my API key be used for?",
     answer:
-      "The API key will be used for accessing an LLM like GPT. If you provide an OpenAI key and use GPT models, your API key will also be used for the default Embeddings model. The embeddings model is extremely cheap. So it shoudn't be too much of a worry. If you use different models than OpenAI then a free embeddings model will be used. Bear in mind that using different embeddings model can also effect the search results",
+      "The API key will be used for accessing an LLM like GPT. If you provide an OpenAI key and use GPT models, your API key will also be used for the default Embeddings model. Embedding models are usually very cheap. If you use different models than OpenAI then a free embeddings model will be used. Bear in mind that using different embeddings model can also effect the search results",
   },
 
   {
-    question: "How can I request new features ?",
+    question: "How can I request new features?",
     answer:
       "To request new features and report bugs, you can reach us at symbiont-me@gmail.com",
   },
@@ -64,7 +64,37 @@ const FAQ = () => {
               </AccordionSummary>
               <Divider />
               <AccordionDetails sx={{ fontWeight: 100 }}>
-                {faq.answer}
+                {faq.question === "Where can I get the API key?" ? (
+                  <>
+                    <p>{faq.answer}</p>
+                    <ul className="pl-4">
+                      <li>
+                        OpenAI:{" "}
+                        <a
+                          href="https://platform.openai.com/api-keys"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="text-blue-500 underline hover:text-blue-800 "
+                        >
+                          https://platform.openai.com/api-keys
+                        </a>
+                      </li>
+                      <li>
+                        Anthropic:{" "}
+                        <a
+                          href="https://docs.anthropic.com/claude/reference/getting-started-with-the-api"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="text-blue-500 underline hover:text-blue-800 "
+                        >
+                          https://docs.anthropic.com/claude/reference/getting-started-with-the-api
+                        </a>
+                      </li>
+                    </ul>
+                  </>
+                ) : (
+                  faq.answer
+                )}
               </AccordionDetails>
             </Accordion>
           </div>
