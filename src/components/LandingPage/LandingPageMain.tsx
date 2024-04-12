9fc10c33fed39f9ea033577d599b17fad3e72e78
lonehegelian <thelonehegelian@protonmail.com>
2024-04-12 17:33:53 +0100

    fix the footer to the landing page

--- src/components/LandingPage/LandingPageMain.tsx
+++ src/components/LandingPage/LandingPageMain.tsx
@@ -3,20 +3,24 @@ import Hero from "./Hero";
 import FAQ from "./FAQ";
 import Footer from "../Footer";
 import { Divider } from "@mui/material";
+import "./styles.css";
 
 const LandingPageMain = () => {
   return (
-    <div className="flex md-screen flex-col overflow-x-hidden">
+    <div className="flex flex-col min-h-screen overflow-x-hidden">
       <Navbar />
       <Divider />
 
-      <div className="flex h-full flex-col md:flex-row">
-        <Hero />
+      <div className="flex flex-grow flex-col">
+        <div className="flex h-full flex-col md:flex-row">
+          <Hero />
+        </div>
+        <Divider />
+        <div className="flex min-h-full w-full flex-col ">
+          <FAQ />
+        </div>
       </div>
-      <Divider />
-      <div className="flex min-h-full w-full flex-col ">
-        <FAQ />
-      </div>
+
       <Footer />
     </div>
   );
