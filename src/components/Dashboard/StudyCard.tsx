659cd4880ade45a7b732d58f59fb8f777e7024ae
lonehegelian <thelonehegelian@protonmail.com>
2024-05-17 14:26:01 +0100

    fix type error

--- src/components/Dashboard/StudyCard.tsx
+++ src/components/Dashboard/StudyCard.tsx
@@ -8,8 +8,7 @@ type StudyCardProps = Study;
 
 // TODO replace img with Image
 
-const StudyCard = ({ name, image, id }: StudyCardProps) => {
-  console.log(id);
+const StudyCard = ({ name, image, _id }: StudyCardProps) => {
   return (
     <div className="project-card">
       <div className="card lg:card-side bg-base-100 shadow-xl">
@@ -20,7 +19,7 @@ const StudyCard = ({ name, image, id }: StudyCardProps) => {
         <div className="card-body">
           <h2 className="card-title">{name}</h2>
           <div className="card-actions justify-end">
-            <Link href={`studies/${id}`} className="btn btn-primary">
+            <Link href={`studies/${_id}`} className="btn btn-primary">
               Open
             </Link>
           </div>
