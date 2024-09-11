dc033232e05a3622bfe503ec471917cb3682fedc
lonehegelian <thelonehegelian@protonmail.com>
2024-09-11 22:19:05 +0100

    add hook for handling redirection in case of not logged in

---
+++ src/hooks/useAuthRedirect.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import Session from "supertokens-auth-react/recipe/session";
+import { UserAuth } from "@/app/context/AuthContext";
+
+const useAuthRedirect = () => {
+  const router = useRouter();
+  const authContext = UserAuth();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    async function checkSession() {
+      const sessionExists = await Session.doesSessionExist();
+      setIsLoggedIn(sessionExists);
+      setLoading(false);
+      setUser(authContext?.user ?? null);
+
+      if (!sessionExists) {
+        router.push("/"); // Redirect to home if not logged in
+      }
+    }
+    checkSession();
+  }, [authContext, router]);
+
+  return { isLoggedIn, loading, user };
+};
+
+export default useAuthRedirect;
