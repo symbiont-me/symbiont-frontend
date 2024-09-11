acc2e9044222987a27b83fa57310da85e4d9f363
lonehegelian <thelonehegelian@protonmail.com>
2024-09-11 23:55:43 +0100

    fix: hydration error and remove unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { UserAuth } from "@/app/context/AuthContext";
 import LandingPage from "@/components/LandingPage/LandingPageMain";
 import { useEffect, useState } from "react";
 import { CircularProgress } from "@mui/material";
-import axios from "axios";
 import Session from "supertokens-auth-react/recipe/session";
 
 export default function Home() {
@@ -20,49 +19,8 @@ export default function Home() {
     }
     checkSession();
   }, []);
-  // useEffect(() => {
-  //   // Assuming `UserAuth` provides a way to check if the auth status is being resolved
-  //   const checkAuthStatus = async () => {
-  //     try {
-  //       if (!authContext) {
-  //         return;
-  //       }
-  //       setLoading(false);
-  //     } catch (error) {
-  //       console.error(error);
-  //       setLoading(false);
-  //     }
-  //   };
 
-  //   checkAuthStatus();
-  // }, [authContext, authContext?.user, authContext?.user?.accessToken, authContext?.user?.id]);
-
-  // async function logIn() {
-  //   const userToken = await authContext?.user?.accessToken;
-
-  //   if (!userToken) {
-  //     return;
-  //   }
-  //   const endpoint = `${process.env.NEXT_PUBLIC_BASE_URL}/login`;
-  //   const body = {};
-  //   const headers = {
-  //     Authorization: `Bearer ${userToken}`,
-  //   };
-
-  //   try {
-  //     const res = await axios.post(endpoint, body, { headers });
-  //     console.log(res);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // }
-
-  if (!authContext) {
-    <div className="h-screen w-screen flex flex-col justify-center items-center">
-      <CircularProgress />
-    </div>;
-  }
-  if (loading) {
+  if (typeof window === "undefined" || !authContext || loading) {
     return (
       <div className="h-screen w-screen flex flex-col justify-center items-center">
         <CircularProgress />
