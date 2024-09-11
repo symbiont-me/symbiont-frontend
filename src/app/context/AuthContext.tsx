d4d9a610c483ede7f0cc6a82a2a5675ef97fbffc
lonehegelian <thelonehegelian@protonmail.com>
2024-09-11 18:04:45 +0100

    update auth context using supertokens

--- src/app/context/AuthContext.tsx
+++ src/app/context/AuthContext.tsx
@@ -1,31 +1,13 @@
 "use client";
-import { ReactNode, useContext, createContext, useState, useEffect } from "react";
+import {
+  ReactNode,
+  useContext,
+  createContext,
+  useState,
+  useEffect,
+} from "react";
 import axios from "axios";
-
-type User = {
-  // User Identifiers
-  id: string; // Unique identifier for each user
-  email?: string; // User's email address, optional
-  phoneNumber?: string; // User's phone number, optional
-
-  // Authentication Details
-  timeJoined: Date; // Timestamp when the user account was created
-  tenantIds?: string[]; // Identifiers for multi-tenancy support, optional
-  thirdPartyLoginInfo?: any; // Details about any third-party authentication methods used, optional
-  loginMethods: string[]; // Information about all the login methods associated with the user
-  emailVerificationStatus: boolean; // Whether the user's email has been verified
-
-  // Session Information
-  accessToken: string; // Short-lived token for accessing protected resources
-  refreshToken?: string; // Used to obtain new access tokens, optional
-
-  // Custom Data
-  metadata?: Record<string, any>; // Additional custom information that can be stored for each user, optional
-
-  // Roles and Permissions
-  defaultRole: string; // Primary role assigned to the user
-  allowedRoles: string[]; // List of roles the user is permitted to have
-};
+import { User } from "../../types";
 
 import { signOut } from "supertokens-auth-react/recipe/session";
 import Session from "supertokens-auth-react/recipe/session";
@@ -35,17 +17,16 @@ type AuthContextType = {
   googleSignIn: () => void;
   googleSignOut: () => void;
   isAuthLoading: boolean;
+  userToken: string | null;
 };
 
 export const AuthContext = createContext<AuthContextType | null>(null);
-
+// TODO this needs to be refactored to remove unnecessary information
 export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
-
   const [isAuthLoading, setIsAuthLoading] = useState(true);
-
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+  const [userToken, setUserToken] = useState<string | null>(null);
   // Check if session exists and set user to null if it doesn't
   useEffect(() => {
     if (Session.getUserId() !== undefined) {
@@ -60,16 +41,27 @@ export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
       }
     })();
   }, []);
+
+  useEffect(() => {
+    async function fetchAccessToken() {
+      const accessToken = await Session.getAccessToken();
+      if (accessToken) {
+        setUserToken(accessToken);
+      }
+    }
+    fetchAccessToken();
+  }, []);
+
+  // @note if access token is null should be an error
   async function login() {
     try {
-      const accessToken = await Session.getAccessToken();
       let response = await axios.post(`http://127.0.0.1:8000/auth/signin`, {
         formFields: [
           { id: "email", value: "retailstash@gmail.com" },
           { id: "password", value: "password_12345" },
         ],
         headers: {
-          Authorization: `Bearer ${accessToken}`,
+          Authorization: `Bearer ${userToken}`,
         },
       });
 
@@ -84,7 +76,9 @@ export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
   async function googleSignIn() {
     await login();
     try {
-      let sessionResponse = await axios.get("http://127.0.0.1:8000/session-details");
+      let sessionResponse = await axios.get(
+        "http://127.0.0.1:8000/session-details"
+      );
       console.log("Session details:", sessionResponse.data);
     } catch (error) {
       console.error("Error fetching session details:", error);
@@ -104,7 +98,9 @@ export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
   }, [user, isLoggedIn]);
 
   return (
-    <AuthContext.Provider value={{ user, googleSignIn, googleSignOut, isAuthLoading }}>
+    <AuthContext.Provider
+      value={{ user, googleSignIn, googleSignOut, isAuthLoading, userToken }}
+    >
       {children}
     </AuthContext.Provider>
   );
