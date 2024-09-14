30e0ab4190933ba664a63bdc324565cad3bb7e07
lonehegelian <thelonehegelian@protonmail.com>
2024-09-14 23:30:51 +0100

    add base url

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com"],
   },
+  env: {
+    NEXT_PUBLIC_BASE_URL: 'http://127.0.0.1:8000'
+  },
+  output: "standalone",
 };
 
 module.exports = nextConfig;
